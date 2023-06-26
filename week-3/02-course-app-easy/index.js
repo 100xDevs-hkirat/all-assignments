aca412718fbeb4b1a0271f68ddd86172bb3a3f5d
Slok Aks <slok.aks204@gmail.com>
2023-06-27 02:13:24 +0530

    implemented 02-course-app-easy

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -1,52 +1,180 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.use(express.json());
+app.use(bodyParser.json());
+
+const ADMIN = 'admin';
+const USER = 'user';
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+let username, password
+
+const authn = (type, headers) => {
+  username = headers.username;
+  password = headers.password;
+
+  switch (type) {
+    case USER: 
+      for(let i = 0; i < USERS.length; i++) {
+        if (USERS[i].username == username) {
+          if (USERS[i].password == password) {
+            return i;
+          }
+        }
+      }
+      break;
+  
+    case ADMIN: 
+      for(let i = 0; i < ADMINS.length; i++) {
+        if (ADMINS[i].username == username) {
+          if (ADMINS[i].password == password) {
+            return i;
+          }
+        }
+      }
+      break;
+
+    default:
+  }
+
+  return -1;
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+  username = req.body.username;
+  password = req.body.password;
+
+  ADMINS.push({
+    username: username,
+    password: password
+  })
+
+  res.json({ message: 'Admin created successfully' })
 });
 
 app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+  let index = authn(ADMIN, req.headers);
+
+  if (index != -1) {
+    return res.json({ message: 'Logged in successfully' });
+  }
+
+  res.sendStatus(401);
 });
 
 app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+  let index = authn(ADMIN, req.headers);
+
+  if (index != -1) {
+    let course = req.body;
+    course.id = Math.floor(Math.random() * 1000000);
+
+    COURSES.push(course);
+
+    return res.json({ message: 'Course created successfully', courseId: course.id })
+  }
+
+  res.sendStatus(401);
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+  let index = authn(ADMIN, req.headers);
+
+  if (index != -1) {
+    let courseId = parseInt(req.params.courseId);
+
+    for (let i = 0; i < COURSES.length; i++) {
+      if (COURSES[i].id == courseId) {
+        COURSES[i] = req.body;
+        COURSES[i].id = courseId;
+
+        return res.json({ message: 'Course updated successfully' });
+      }
+    }
+
+    return res.sendStatus(404);
+  }
+
+  res.sendStatus(401);
 });
 
 app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+  let index = authn(ADMIN, req.headers);
+
+  if (index != -1) {
+    return res.json({courses: COURSES});
+  }
+
+  res.sendStatus(401);
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+  username = req.body.username;
+  password = req.body.password;
+
+  USERS.push({
+    username: username,
+    password: password,
+    purchasedCourses: []
+  })
+
+  res.json({ message: 'User created successfully' })
 });
 
 app.post('/users/login', (req, res) => {
-  // logic to log in user
+  let index = authn(USER, req.headers);
+
+  if (index != -1) {
+    return res.json({ message: 'Logged in successfully' });
+  }
+
+  res.sendStatus(401);
 });
 
 app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+  let index = authn(USER, req.headers);
+
+  if (index != -1) {
+    return res.json({courses: COURSES});
+  }
+
+  res.sendStatus(401);
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+  let index = authn(USER, req.headers);
+
+  if (index != -1) {
+    let courseId = parseInt(req.params.courseId);
+
+    for (let i = 0; i < COURSES.length; i++) {
+      if (COURSES[i].id == courseId) {
+        USERS[index].purchasedCourses.push(COURSES[i]);
+        return res.json({ message: 'Course purchased successfully' });
+      }
+    }
+
+    return res.sendStatus(404);
+  }
+
+  res.sendStatus(401);
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+  let index = authn(USER, req.headers);
+
+  if (index != -1) {
+    return res.json({purchasedCourses: USERS[index].purchasedCourses})
+  }
+
+  res.sendStatus(401);
 });
 
 app.listen(3000, () => {
