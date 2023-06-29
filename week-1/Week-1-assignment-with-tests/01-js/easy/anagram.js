defc1973e186ab18fe120f0c8ac09d4276d5292c
jack <jackcruiser800@gmail.com>
2023-06-29 17:44:34 +0530

    solution for anagram

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,12 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.toLowerCase().split("").sort().join("") === str2.toLowerCase().split("").sort().join(""))
+  return true;
+  else
+  return false;
 
 }
+
 
 module.exports = isAnagram;
