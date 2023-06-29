337d5a8b34a7e97bad1a512afdc17ef59f0cb8ef
kunal00000 <kunalverma2468@gmail.com>
2023-06-30 03:03:55 +0530

    course app file data

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,54 +1,248 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
+const coursePath = path.join(__dirname, "./COURSES.json");
+const adminPath = path.join(__dirname, "./ADMINS.json");
+const userPath = path.join(__dirname, "./USERS.json");
+const purchasePath = path.join(__dirname, "./PURCHASED_COURSES.json");
+
+function readfile(Path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(Path, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+}
+
 let COURSES = [];
+readfile(coursePath)
+  .then((data) => {
+    COURSES = data;
+  })
+  .catch((err) => console.log(err));
+
+let ADMINS = [];
+readfile(adminPath)
+  .then((data) => {
+    ADMINS = data;
+  })
+  .catch((err) => console.log(err));
+
+let USERS = [];
+readfile(userPath)
+  .then((data) => {
+    USERS = data;
+  })
+  .catch((err) => console.log(err));
+
+let PURCHASED_COURSES = [];
+readfile(purchasePath)
+  .then((data) => {
+    PURCHASED_COURSES = data;
+  })
+  .catch((err) => console.log(err));
+
+function writefile(Path, Data) {
+  fs.writeFile(Path, Data, "utf-8", (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+app.post("/admin/signup", (req, res) => {
+  const { username, password } = req.body;
+  const isUsername = ADMINS.find((admin) => admin.username === username);
+
+  if (isUsername) {
+    res.status(400).json("Username exists");
+  } else {
+    ADMINS.push({
+      username: username,
+      password: password,
+    });
+    writefile(adminPath, JSON.stringify(ADMINS, null, 2));
+    res.status(201).json({ message: "Admin created succesfully" });
+  }
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post("/admin/login", (req, res) => {
+  const { username, password } = req.headers;
+  const foundAdmin = ADMINS.find((admin) => {
+    return admin.username === username && admin.password === password;
+  });
+
+  if (foundAdmin) {
+    res.status(200).json({ message: "Logged in successfully" });
+  } else {
+    res.status(404).json({ message: "Admin Not Found" });
+  }
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+app.post("/admin/courses", (req, res) => {
+  const { username, password } = req.headers;
+  const foundAdmin = ADMINS.find((admin) => {
+    return admin.username === username && admin.password === password;
+  });
+
+  if (!foundAdmin) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    if (COURSES.length) {
+      var previous_id = COURSES.at(COURSES.length - 1).id;
+    }
+
+    const id = previous_id ? previous_id + 1 : 1;
+    COURSES.push({
+      id: id,
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      imageLink: req.body.imageLink,
+      published: req.body.published,
+    });
+    writefile(coursePath, JSON.stringify(COURSES, null, 2));
+    res
+      .status(201)
+      .json({ message: "Course created successfully", courseId: id });
+  }
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+app.put("/admin/courses/:courseId", (req, res) => {
+  const { username, password } = req.headers;
+  const foundAdmin = ADMINS.find((admin) => {
+    return admin.username === username && admin.password === password;
+  });
+
+  if (!foundAdmin) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    const idx = COURSES.findIndex((course) => {
+      return course.id == req.params.courseId;
+    });
+
+    if (idx === -1) {
+      res.status(404).json({ message: "Course Not Found" });
+    } else {
+      COURSES[idx].title = req.body.title;
+      COURSES[idx].description = req.body.description;
+      COURSES[idx].price = req.body.price;
+      COURSES[idx].imageLink = req.body.imageLink;
+      COURSES[idx].published = req.body.published;
+      writefile(coursePath, JSON.stringify(COURSES, null, 2));
+      res.status(200).json({ message: "Course updated succesfully" });
+    }
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+app.get("/admin/courses", (req, res) => {
+  const { username, password } = req.headers;
+  const foundAdmin = ADMINS.find((admin) => {
+    return admin.username === username && admin.password === password;
+  });
+
+  if (!foundAdmin) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    res.status(200).json({ courses: COURSES });
+  }
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+app.post("/users/signup", (req, res) => {
+  const isUsername = USERS.find((user) => user.username === req.body.username);
+
+  if (isUsername) {
+    res.status(400).json({ message: "Username exists" });
+  } else {
+    USERS.push({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    writefile(userPath, JSON.stringify(USERS, null, 2));
+    res.status(201).json({ message: "User created succesfully" });
+  }
 });
 
-app.post('/users/login', (req, res) => {
-  // logic to log in user
+app.post("/users/login", (req, res) => {
+  const foundUser = USERS.find((user) => {
+    return (
+      user.username === req.headers.username &&
+      user.password === req.headers.password
+    );
+  });
+
+  if (!foundUser) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    res.status(200).json({ message: "Logged in succesfully" });
+  }
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+app.get("/users/courses", (req, res) => {
+  const foundUser = USERS.find((user) => {
+    return (
+      user.username === req.headers.username &&
+      user.password === req.headers.password
+    );
+  });
+
+  if (!foundUser) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    res.status(200).json({ courses: COURSES });
+  }
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+app.post("/users/courses/:courseId", (req, res) => {
+  const foundUser = USERS.find((user) => {
+    return (
+      user.username === req.headers.username &&
+      user.password === req.headers.password
+    );
+  });
+
+  if (!foundUser) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    const purchase_course = COURSES.find(
+      (course) => course.id == req.params.courseId
+    );
+    if (!purchase_course) {
+      res.status(404).json({ message: "Course not found" });
+    } else {
+      PURCHASED_COURSES.push(purchase_course);
+      writefile(purchasePath, JSON.stringify(PURCHASED_COURSES, null, 2));
+      res.status(200).json({ message: "Course purchased successfully" });
+    }
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+app.get("/users/purchasedCourses", (req, res) => {
+  console.log(USERS);
+  const foundUser = USERS.find((user) => {
+    return (
+      user.username === req.headers.username &&
+      user.password === req.headers.password
+    );
+  });
+
+  if (!foundUser) {
+    res.status(403).json({ message: "Forbidden or Unauthorised" });
+  } else {
+    res.status(200).json({ purchasedcourse: PURCHASED_COURSES });
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("crush it on port 3000");
 });
