71b584d8ac934f03d3c3087512c589762971c7bf
dileepkumar1228 <123911650+dileepkumar1228@users.noreply.github.com>
2023-06-30 11:33:10 -0400

    question1ans2solutions

--- week-3/01-reconciler/script.js
+++ week-3/01-reconciler/script.js
@@ -1,32 +1,61 @@
 
 // What are u looking for?
 // You'll get it on Friday you lazy porson
+var global_ids = [];
+var new_ids = [];
+
+
+function findArrayDifference(array1, array2) {
+  return array1.filter((element) => !array2.includes(element));
+}
 
 function createDomElements(data) {
-  var parentElement = document.getElementById("mainArea");
+  // console.log(data.length);
 
-  // Get the current children of the parent element and convert it to an array
-  var currentChildren = Array.from(parentElement.children);
+  for(var i=0;i<data.length;i++){
+    new_ids.push(data[i].id);
+  }
 
-  // Process each item in the data array
-  data.forEach(function(item) {
-    // Check if a child with this ID already exists
-    var existingChild = currentChildren.find(function(child) {
-      return child.dataset.id === String(item.id);
-    });
+  var To_be_removed = findArrayDifference(global_ids, new_ids);
+  var to_be_added = findArrayDifference(new_ids, global_ids);
 
-    if (existingChild) {
-      // If it exists, update it
-      existingChild.children[0].innerHTML = item.title;
-      existingChild.children[1].innerHTML = item.description;
-      // Remove it from the currentChildren array
-      currentChildren = currentChildren.filter(function(child) {
-        return child !== existingChild;
-      });
-    } else {
-      // If it doesn't exist, create it
+  // console.log("global_ids"); 
+
+  // console.log(global_ids); 
+
+  // console.log("new_ids");
+
+  // console.log(new_ids);
+
+  // console.log("To_be_removed");
+
+  // console.log(To_be_removed);
+
+  // console.log("to_be_added");
+
+  // console.log(to_be_added);
+
+  // console.log(to_be_added);
+
+    var parentElement = document.getElementById("mainArea");
+  
+    for (var i = 0; i < To_be_removed.length; i++) {
+      var childElementToRemove = parentElement.querySelector("[data-id='" + To_be_removed[i] + "']");
+      if (childElementToRemove) {
+        parentElement.removeChild(childElementToRemove);
+      }
+      global_ids.pop(To_be_removed[i]);
+    }
+    
+    
+
+  for(var i=0;i<to_be_added.length;i++){
+    if(!global_ids.includes(to_be_added[i])){
+      global_ids.push(to_be_added[i]);
+      var item = to_be_added[i];
+
       var childElement = document.createElement("div");
-      childElement.dataset.id = item.id; // Store the ID on the element for future lookups
+      childElement.dataset.id = item; // Store the ID on the element for future lookups
 
       var grandChildElement1 = document.createElement("span");
       grandChildElement1.innerHTML = item.title
@@ -36,22 +65,21 @@ function createDomElements(data) {
 
       var grandChildElement3 = document.createElement("button");
       grandChildElement3.innerHTML = "Delete"
-      grandChildElement3.setAttribute("onclick", "deleteTodo(" + item.id + ")")
+      grandChildElement3.setAttribute("onclick", "deleteTodo(" + item + ")")
 
       childElement.appendChild(grandChildElement1)
       childElement.appendChild(grandChildElement2)
       childElement.appendChild(grandChildElement3)
       parentElement.appendChild(childElement);
-    }
-  });
 
-  // Any children left in the currentChildren array no longer exist in the data, so remove them
-  currentChildren.forEach(function(child) {
-    parentElement.removeChild(child);
-  });
+    }
+  }
+  new_ids = [];
 }
 
-//
+// Math.floor(Math.random() * 100)
+
+
 // window.setInterval(() => {
 //   let todos = [];
 //   for (let i = 0; i<Math.floor(Math.random() * 100); i++) {
@@ -61,6 +89,7 @@ function createDomElements(data) {
 //       id: i+1
 //     })
 //   }
-//
+
 //   createDomElements(todos)
 // }, 1000)
+
--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,46 +7,299 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+
+// POST /admin/signup
+//    Description: Creates a new admin account.
+//    Input: { username: 'admin', password: 'pass' }
+//    Output: { message: 'Admin created successfully' }
+
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post('/admin/signup', (request, response) => {
   // logic to sign up admin
+  var data = request.body;
+  var obj = {
+      username : data.username,
+      password : data.password,
+  }
+  ADMINS.push(obj);
+  response.send({ message: 'Admin created successfully' });
+
 });
 
-app.post('/admin/login', (req, res) => {
+// POST /admin/login
+//    Description: Authenticates an admin. It requires the admin to send username and password in the headers.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Output: { message: 'Logged in successfully' }
+
+app.post('/admin/login', (request, response) => {
   // logic to log in admin
+  var data = request.headers;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isadmin = ADMINS.some(function(admin){
+    return admin.password === password && admin.username === username;
+  })
+
+  if(isadmin){
+    response.send({ message: 'Logged in successfully' });
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
+
 });
 
-app.post('/admin/courses', (req, res) => {
+// POST /admin/courses
+//    Description: Creates a new course.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Input: Body: { title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }
+//    Output: { message: 'Course created successfully', courseId: 1 }
+
+app.post('/admin/courses', (request, response) => {
   // logic to create a course
+
+  var data = request.headers;
+  var courses_data = request.body;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isadmin = ADMINS.some(function(admin){
+    return admin.password === password && admin.username === username;
+  })
+
+  if(isadmin){
+    var obj = {
+      id : COURSES.length + 1,
+      title : courses_data.title,
+      description : courses_data.description,
+      price : courses_data.price,
+      imageLink : courses_data.imageLink,
+      published : courses_data.published
+    }
+    COURSES.push(obj);
+    response.send({ message: 'Course created successfully', "courseId":  obj.id });
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
+
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+// PUT /admin/courses/:courseId
+//    Description: Edits an existing course. courseId in the URL path should be replaced with the ID of the course to be edited.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Input: Body { title: 'updated course title', description: 'updated course description', price: 100, imageLink: 'https://updatedlinktoimage.com', published: false }
+//    Output: { message: 'Course updated successfully' }
+
+app.put('/admin/courses/:courseId', (request, response) => {
   // logic to edit a course
+  var data = request.headers;
+  var courseId = request.params;
+  var course_data = request.body;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isadmin = ADMINS.some(function(admin){
+    return admin.password === password && admin.username === username;
+  });
+
+  if(isadmin){
+    COURSES.forEach((course)=>{
+      if(course.id == courseId){
+
+        course.title = course_data.title;
+        course.description = course_data.description;
+        course.price = course_data.price;
+        course.imageLink = course_data.imageLink;
+        course.published = course_data.published;
+        
+      }
+    });
+
+    response.send( { message: 'Course updated successfully' });
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user' });
+  }
+
 });
 
-app.get('/admin/courses', (req, res) => {
+
+// GET /admin/courses
+//    Description: Returns all the courses.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, 
+//    imageLink: 'https://linktoimage.com', published: true }, ... ] }
+
+
+app.get('/admin/courses', (request, response) => {
   // logic to get all courses
+
+  var data = request.headers;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isadmin = ADMINS.some(function(admin){
+    return admin.password === password && admin.username === username;
+  });
+
+  if(isadmin){
+    response.status(200).json(COURSES);
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user' });
+  }
+
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+
+// POST /users/signup
+//    Description: Creates a new user account.
+//    Input: { username: 'user', password: 'pass' }
+//    Output: { message: 'User created successfully' } 
+
+app.post('/users/signup', (request, response) => {
   // logic to sign up user
+
+  var data = request.body;
+  var obj = {
+      username : data.username,
+      password : data.password,
+      courses_bought : [],
+  }
+  USERS.push(obj);
+  response.send({ message: 'User created successfully' } );
 });
 
-app.post('/users/login', (req, res) => {
+
+// POST /users/login
+//    Description: Authenticates a user. It requires the user to send username and password in the headers.
+//    Input: Headers: { 'username': 'user', 'password': 'pass' }
+//    Output: { message: 'Logged in successfully' }
+
+app.post('/users/login', (request, response) => {
   // logic to log in user
+
+  var data = request.headers;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isUser = USERS.some(function(user){
+    return user.password === password && user.username === username;
+  })
+
+  if(isUser){
+    response.send({ message: 'Logged in successfully' });
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
+
 });
 
-app.get('/users/courses', (req, res) => {
+
+// GET /users/courses
+//    Description: Lists all the courses.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+
+app.get('/users/courses', (request, response) => {
   // logic to list all courses
+
+  var data = request.headers;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isUser = USERS.some(function(user){
+    return user.password === password && user.username === username;
+  })
+
+  if(isUser){
+    response.status(200).json(COURSES);
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
+
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+// POST /users/courses/:courseId
+//    Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Output: { message: 'Course purchased successfully' }
+
+app.post('/users/courses/:courseId', (request, response) => {
   // logic to purchase a course
+
+  var data = request.headers;
+
+  var courseId = request.params.courseId;
+
+  var username = data.username;
+  var password = data.password;
+
+  var isUser = USERS.some(function(user){
+    return user.password === password && user.username === username;
+  })
+
+  if(isUser){
+    
+    USERS.some(function(user){
+      if( user.password === password && user.username === username ){
+        user.courses_bought.push(courseId);
+      }
+    });
+    response.status(200).send({ message: 'Course purchased successfully' });
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
+
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+// GET /users/purchasedCourses
+//    Description: Lists all the courses purchased by the user.
+//    Input: Headers: { 'username': 'admin', 'password': 'pass' }
+//    Output: { purchasedCourses: [ { id: 1, title: 'course title', description: 'course description', 
+//    price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+
+app.get('/users/purchasedCourses', (request, response) => {
   // logic to view purchased courses
+
+  var data = request.headers;
+
+  var username = data.username;
+  var password = data.password;
+
+  var purchasedCourses = [];
+
+  var isUser = USERS.some(function(user){
+    if( user.password === password && user.username === username){
+      purchasedCourses = user.courses_bought || [];
+      return true;
+    }
+    else{
+      return false;
+    }
+  });
+
+
+
+  if(isUser){
+    var sol = COURSES.filter( (course) => {purchasedCourses.includes(course.id)});
+    response.status(200).json(sol);
+  }
+  else{
+    response.status(404).send({ message: 'Unauthorized user ' });
+  }
 });
 
 app.listen(3000, () => {
