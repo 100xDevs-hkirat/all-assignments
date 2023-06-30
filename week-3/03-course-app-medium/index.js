588bf0cd7cc2dbc40be7859e4a8e2376a8b1bac1
haki-user <adityapratapsingh33@outlook.com>
2023-06-30 17:08:21 +0530

    modified:   week-3/02-course-app-easy/README.md 	new file:   week-3/03-course-app-medium/admins.json 	new file:   week-3/03-course-app-medium/courses.json 	modified:   week-3/03-course-app-medium/index.js 	new file:   week-3/03-course-app-medium/purchase.json 	new file:   week-3/03-course-app-medium/users.json

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,15 +1,68 @@
 const express = require('express');
+const fs = require('node:fs');
+
 const app = express();
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+// let ADMINS = [];
+// let USERS = [];
+// let COURSES = [];
+
+// Authentication
+const adminAuthentication = (req, res, next) => {
+  next();
+}
+const userAuthentication = (req, res, next) => {
+  next();
+}
+
+const getFileData = (fileName, callback) => {
+  fs.readFile(fileName, 'utf-8', (err, fileData) => {
+    if(err) {
+      console.error(err);
+      callback([]);
+    } else {
+      try {
+        const data = JSON.parse(fileData);
+        callback(data);
+      } catch(err) {
+        console.error("Error parsing file data:", err);
+        callback([]);
+      }
+    }
+  });
+};
+
+const writeFile = (fileName, data, callback) => {
+  fs.writeFile(fileName, JSON.stringify(data), (err) => {
+    if(err) {
+      console.error("Error writing to file:", err);
+      callback(err);
+    }
+    if(callback) {
+      callback();
+    }
+  });
+};
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+  const admin = req.body;
+  getFileData('admins.json', (admins) => {
+    const adminExists = admins.find(a => a.username === admin.username);
+    if(adminExists) {
+      res.send(403).json({ message: "Admin already exists" });
+    } else {
+      admins.push(admin);
+      writeFile('admins.json', admins, (err) => {
+        if(err) {
+          return res.status(500);
+        }
+        res.json("")
+      })
+    }
+  })
 });
 
 app.post('/admin/login', (req, res) => {
