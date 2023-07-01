ffda5e1b182cbde380892bb1ac4759de02561ea3
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-01 18:24:12 +0530

    Week 01 Day 02 HARD

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/3-promise-all.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,42 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log('waitOneSecond');
+      resolve(1000);
+    }, 1000)
+  );
 }
 
 function waitTwoSecond() {
-
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log('waitTwoSecond');
+      resolve(2000);
+    }, 2000)
+  );
 }
 
 function waitThreeSecond() {
-
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log('waitThreeSecond');
+      resolve(3000);
+    }, 3000)
+  );
 }
 
 function calculateTime() {
-
+  const startTime = Date.now();
+  console.log(startTime);
+  const promises = [waitOneSecond(), waitTwoSecond(), waitThreeSecond()];
+  Promise.all(promises).then((result) => {
+    console.log(result);
+    const endTime = Date.now();
+    console.log((endTime - startTime) / 1000);
+  });
 }
+
+calculateTime();
