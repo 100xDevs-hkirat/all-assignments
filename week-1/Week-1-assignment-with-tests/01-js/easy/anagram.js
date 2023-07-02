e3e691abd73e01053f7135173d275f7dfe22592c
kunal-arya <kunalaryad@gmail.com>
2023-07-02 16:55:35 +0530

    Week 1 01-js assignments completed

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,22 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  for (let i = 0; i < str1.length; i++) {
+    if (!str2.includes(str1[i]) && !str1.includes(str2[i])) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+isAnagram("openai!", "open");
 
 module.exports = isAnagram;
