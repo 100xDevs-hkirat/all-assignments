8f944b79e9c7ba44cb0ba75cc9b3ca7b4e87b9ae
Nilay Banerjee <66667272+NooBIE-Nilay@users.noreply.github.com>
2023-07-03 18:45:33 +0530

    Solved times.js

--- week-1/Week-1-assignment-with-tests/01-js/medium/times.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const start_time_in_ms = Date.now();
+  let sum = 0;
+  for (let i = 1; i <= n; i++) sum += i;
+  const end_time_in_ms = Date.now();
+  return (end_time_in_ms - start_time_in_ms) / 1000;
 }
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
