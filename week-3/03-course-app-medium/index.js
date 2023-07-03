d86eafaea083325d59a79cbca103f1a7a04ba520
bharath <bharathprasannan96@gmail.com>
2023-07-03 14:02:33 +0530

    first commit

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const { Admin } = require("mongodb");
 const { parseArgs } = require("util");
-=======
-const express = require('express');
-const app = express();
->>>>>>> source/main
 
 app.use(express.json());
 
@@ -16,7 +11,6 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-<<<<<<< HEAD
 const secretKey = "adm1nS3c38";
 
 try {
@@ -214,50 +208,4 @@ app.get("/users/purchasedCourses", authenticateJwt,(req, res) => {
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
-=======
-// Admin routes
-app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
-});
-
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
-});
-
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
-});
-
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
-});
-
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
-});
-
-// User routes
-app.post('/users/signup', (req, res) => {
-  // logic to sign up user
-});
-
-app.post('/users/login', (req, res) => {
-  // logic to log in user
-});
-
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
-});
-
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
-});
-
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
-});
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
->>>>>>> source/main
-});
+})
