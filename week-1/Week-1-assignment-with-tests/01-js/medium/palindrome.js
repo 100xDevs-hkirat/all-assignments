618ac3aee8e5d56207879797fb2b82a04610fb38
Durgesh Nishad <dnishad042@gmail.com>
2023-07-04 23:42:33 +0530

    solved palindrome.js

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -6,8 +6,32 @@
   - `npm run test-palindrome`
 */
 
+function reverse(str){
+  var revstr="";
+  for(var i= str.length-1;i>=0;i--){
+    revstr+=str[i];
+  }
+  return revstr;
+}
+
+function correction (str){
+  var cstr="";
+  for(var i=0;i<str.length;i++){
+    if(str.charCodeAt(i)>=33 && str.charCodeAt(i)<=64 || str.charCodeAt(i)>=91 && str.charCodeAt(i)<=96 || str.charCodeAt(i)>=123 && str.charCodeAt(i)<=126 || str[i]==' '){}
+    else 
+      cstr+=str[i];
+  }
+  return cstr;
+}
+
 function isPalindrome(str) {
-  return true;
+  var cstr= correction(str);
+  cstr=cstr.toLowerCase();
+  var reversedString= reverse(cstr);
+  if (cstr==reversedString)
+    return true;
+  else 
+    return false;
 }
 
 module.exports = isPalindrome;
