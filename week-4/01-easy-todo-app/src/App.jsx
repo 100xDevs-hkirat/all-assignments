c4f173487f1ca750e7f3c637a59769b5bb8de97c
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-04 23:55:31 +0530

    Update Code

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  const [todos, setTodos] = useState(['Morning Walk']);
-  // fetch all todos from server
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/todos')
+      .then((res) => res.json())
+      .then((result) => setTodos(result));
+  }, []);
+
   const addTodo = () => {
     console.log('Add clicked');
   };
@@ -21,8 +28,11 @@ function App() {
         <button onClick={addTodo}>Add</button>
       </div>
       {todos.map((todo) => (
-        <div>
-          <p>{todo}</p>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+          </div>
           <button onClick={removeTodo}>x</button>
         </div>
       ))}
