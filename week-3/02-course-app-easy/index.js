e5fa35b1eddba52bbf74a03513819ed0f0b01a49
Akash_gahlot <akashgahlot1926@gmail.com>
2023-07-04 20:25:06 +0530

    admin signup and login code

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -8,47 +8,77 @@ let USERS = [];
 let COURSES = [];
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post("/admin/signup", (req, res) => {
   // logic to sign up admin
+  const data = {
+    isAdmin: true,
+    username: req.headers.username,
+    password: req.headers.password,
+  };
+  if (ADMINS.push(data) && data.username) {
+    return res.status(200).send("Admin Created successfulyy");
+  } else {
+    return res
+      .status(401)
+      .send(
+        "Some error while creating admin , please try again after sometime"
+      );
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post("/admin/login", (req, res) => {
   // logic to log in admin
+  const cred = {
+    username: req.headers.username,
+    password: req.headers.password,
+  };
+  console.log(req.headers.username);
+  const index = ADMINS.findIndex((item) => item.username == cred.username);
+  console.log(index);
+  if (index != -1 && ADMINS[index].isAdmin) {
+    if (ADMINS[index].password != cred.password) {
+      return res.status(404).send("Invalid Password");
+    } else {
+      res.status(200).send("Admin logged successfully ");
+    }
+  } else {
+    return res.status(404).send("Such Admin doesn't exist , please signup");
+  }
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post("/admin/courses", (req, res) => {
   // logic to create a course
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", (req, res) => {
   // logic to edit a course
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", (req, res) => {
   // logic to get all courses
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", (req, res) => {
   // logic to sign up user
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", (req, res) => {
   // logic to log in user
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", (req, res) => {
   // logic to list all courses
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", (req, res) => {
   // logic to purchase a course
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", (req, res) => {
   // logic to view purchased courses
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
