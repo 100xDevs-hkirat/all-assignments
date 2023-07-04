f654cbcdcc33d30dde608b8974c29f6918ceaf0c
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-04 23:40:26 +0530

    Add Week 04 Todo

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  const [todos, setTodos] = useState(['Morning Walk']);
+  // fetch all todos from server
+  const addTodo = () => {
+    console.log('Add clicked');
+  };
 
+  const removeTodo = () => {
+    console.log('Remove clicked');
+  };
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        <input type='text' />
+        <button onClick={addTodo}>Add</button>
       </div>
+      {todos.map((todo) => (
+        <div>
+          <p>{todo}</p>
+          <button onClick={removeTodo}>x</button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
 function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
+  // Add a delete button here so user can delete a TODO.
+  return <div>{props.title}</div>;
 }
 
-export default App
+export default App;
