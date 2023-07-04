08308011d24b5c8a565f3dd79d07928a4f0a0283
Prabhat Dwivedi <96712395+dwi11harsh@users.noreply.github.com>
2023-07-05 04:29:32 +0530

    Update index.js

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,54 +1,167 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const port = 3000;
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+const SECRET = "da-sekret-key";
 
-// Admin routes
-app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+//schemas
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean,
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+//models
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+mongoose.connect(
+  "mongodb+srv://dwiHarsh:harsh411@cluster0.vwmefrc.mongodb.net/courses"
+);
+
+// Admin Signup
+app.post("/admin/signup", async (req, res) => {
+  let { username, password } = req.body;
+  const admin = await Admin.findOne({ username });
+
+  if (admin) return res.status(403).send();
+
+  let newAdmin = new Admin({ username, password });
+  await newAdmin.save();
+  const token = jwt.sign({ username, role: "admin" }, SECRET, {
+    expiresIn: "1h",
+  });
+  res.json({ message: "Admin created successfully", token });
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+// Admin Login
+app.post("/admin/login", (req, res) => {
+  let { username, password } = req.body;
+
+  const admin = Admin.findOne({ username, password });
+
+  if (admin) {
+    const token = jwt.sign(
+      ({ username, role: "admin" }, SECRET, { expiresIn: "1h" })
+    );
+    res.json({ message: "Logged in successfullyy", token });
+  } else res.status(403).json({ message: "invalid credentials" });
 });
 
-// User routes
-app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+//create a course
+app.post("/admin/courses", authenticateJwt, async (req, res) => {
+  const course = new Course(req.body);
+  await Course.save();
+  res.json({ message: "Coourse created successfully", courseId: course.id });
 });
 
-app.post('/users/login', (req, res) => {
-  // logic to log in user
+app.put("/admin/courses/:courseId", authenticateJwt, async (req, res) => {
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+    new: true,
+  });
+  if (course) {
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+// Get all courses
+app.get("/admin/courses", authenticateJwt, (req, res) => {
+  const courses = Course.find({});
+  res.json({ courses });
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+// User Signup
+app.post("/users/signup", async (req, res) => {
+  let { username, password } = req.headers;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    return res.status(403).json({ message: "User already exists" });
+  } else {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "User created successfully", token });
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+// User Login
+app.post("/users/login", (req, res) => {
+  let { username, password } = req.body;
+
+  const user = User.findOne({ username, password });
+  if (user) {
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Logged in successfully", token });
+  } else res.status(403).json({ message: "invalid credentials" });
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// Get all courses
+app.get("/users/courses", authenticateJwt, async (req, res) => {
+  const courses = await Course.find({ published: true });
+  res.json({ courses });
+});
+
+//purchase a course by id
+app.post("users/courses/:courseId", authenticateJwt, async (req, res) => {
+  const course = Course.findById(req.params.courseId);
+  if (course) {
+    const user = await Course.findOne({ username: req.user.username });
+    if (user) {
+      user.purchasedCourses.push(course);
+      await user.save();
+      res.json({ message: "Course purchased successfully" });
+    } else res.status(404).json({ message: "User not found" });
+  } else res.status(404).json({ message: "Course not found" });
+});
+
+app.get("/user/purchasedCourses", authenticateJwt, async (req, res) => {
+  const user = await User.findOne({ username: req.user.username }).populate(
+    "purchasedCourses"
+  );
+  if (user) {
+    res.json({ purchasedCourses: user.purchasedCourses || [] });
+  } else res.status(403).json({ messsage: "User not found" });
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
