0238654ddc03cca250b8e14fc631677c8fc04f80
Tushar Jaiswal <tusharj045@gmail.com>
2023-07-05 18:19:17 +0530

    completed week4 01-easy

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './pages/Todo';
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
-
   return (
     <>
       <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
+        <Todo />
       </div>
     </>
   )
-}
-
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
 }
 
 export default App
