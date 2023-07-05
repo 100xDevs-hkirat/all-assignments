75a28dd17447039964cd35c426853b6dc217ab99
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-07-06 00:51:32 +0530

    Week 4

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function useTodos() {
+  const [todos, setTodos] = useState([]);
+  // fetch all todos from server
+  React.useEffect(() => {
+    fetch("http://localhost:3000/todos", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  return todos;
+}
+
+function ListallTodos() {
+  const todos = useTodos();
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.title}</div>
+          <div>{todo.description}</div>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
-
   return (
-    <>
-      <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
-      </div>
-    </>
-  )
+    <div>
+      <ListallTodos></ListallTodos>
+    </div>
+  );
 }
 
 function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
+  // Add a delete button here so user can delete a TODO.
+  return <div>{props.title}</div>;
 }
 
-export default App
+export default App;
