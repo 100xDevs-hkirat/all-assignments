a914212ac538a05ddbc2be5e76ffae6dc6314ae5
Pralay <pralaydutta666.pd@gmail.com>
2023-07-06 00:42:34 +0530

    completed the week-4 easy

---
+++ week-4/Solution01/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import Todo from "./components/todo";
+
+const App = ()=>{
+  return (
+    <div>
+      <Todo />
+    </div>
+  )
+};
+
+
+export default App;
