92425382b9aa6fd14dba55427b53980210a89231
jagdish.rajput <jagdishsingh13rajput@gmail.com>
2023-07-05 16:26:20 -0400

    easy course app done

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -1,35 +1,103 @@
 const express = require("express");
 //const bodyparser = require("body-parser");
 const app = express();
-
+const fs = require("fs");
 app.use(express.json());
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-const loginMiddleware = (req, res, next) => {
-  let { username, password } = req.headers;
-  let adminId = ADMINS.findIndex((admin) => admin.username === username);
+/* const writeData = (fileName, data) => {
+  fs.writeFile(`./files/${fileName}`, JSON.stringify(data), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}; */
 
-  if (ADMINS.findIndex((admin) => admin.username === username) == -1) {
+/* const readData = (fileName) => {
+  fs.readFile(`./files/${fileName}`, "utf8", (err1, content) => {
+    if (err1) {
+      console.log("Error while reading contents of the folder: " + err1);
+      return null;
+    } else {
+      //console.log(JSON.parse(content));
+      //console.log("Content " + content);
+      content = JSON.parse(content);
+      if (content.length > 0) {
+        switch (fileName) {
+          case "ADMINS.txt":
+            ADMINS = content;
+            break;
+          case "USERS.txt":
+            USERS = content;
+            break;
+          case "COURSES.txt":
+            COURSES = content;
+            break;
+        }
+      }
+      //console.log(TODOS);
+    }
+  });
+};
+
+readData("ADMINS.txt", ADMINS);
+readData("USERS.txt", USERS);
+readData("COURSES.txt", COURSES);
+
+ */ /* console.log("Admins " + JSON.stringify(ADMINS));
+console.log("Users " + JSON.stringify(USERS));
+console.log("Courses " + JSON.stringify(COURSES));
+ */
+const loginMiddlewareAdmin = (req, res, next) => {
+  let { username, password } = req.headers;
+  let adminId = -1;
+
+  if (ADMINS.length > 0)
+    adminId = ADMINS.findIndex((admin) => admin.username === username);
+
+  if (adminId === -1) {
     return res.status(409).send("User doesn't exist");
   } else if (
     ADMINS[adminId].username === username &&
     ADMINS[adminId].password === password
   ) {
     next();
+  } else {
+    return res.status(403).send("User Id or Password is incorrect");
   }
-
-  return res.status(403).send("User Id or Password is incorrect");
 };
 
-const isAdmin = (req, res, next) => {
-  if (ADMINS.findIndex((admin) => admin.username === res.body.username) != -1) {
+const loginMiddlewareUser = (req, res, next) => {
+  let { username, password } = req.headers;
+  let userId = -1;
+
+  if (USERS.length > 0)
+    userId = USERS.findIndex((user) => user.username === username);
+
+  if (userId === -1) {
+    return res.status(409).send("User doesn't exist");
+  } else if (
+    USERS[userId].username === username &&
+    USERS[userId].password === password
+  ) {
     next();
+  } else {
+    return res.status(403).send("User Id or Password is incorrect");
   }
-  return res.status(403).send("You are not an admin");
 };
+
+/* const isAdmin = (req, res, next) => {
+  if (
+    ADMINS.findIndex((admin) => admin.username === req.headers.username) != -1
+  ) {
+    next();
+  } else {
+    return res.status(403).send("You are not an admin");
+  }
+}; */
 // Admin routes
 app.post("/admin/signup", (req, res) => {
   // logic to sign up admin
@@ -40,15 +108,16 @@ app.post("/admin/signup", (req, res) => {
   }
 
   ADMINS.push({ username: username, password: password });
+  //writeData("ADMINS.txt", ADMINS);
   return res.status(200).json({ message: "Admin created successfully" });
 });
 
-app.post("/admin/login", loginMiddleware, isAdmin, (req, res) => {
+app.post("/admin/login", loginMiddlewareAdmin, (req, res) => {
   // logic to log in admin
   return res.status(200).json({ message: "Admin Logged in successfully" });
 });
 
-app.post("/admin/courses", loginMiddleware, isAdmin, (req, res) => {
+app.post("/admin/courses", loginMiddlewareAdmin, (req, res) => {
   // logic to create a course
   //Body: { title: 'course title', description: 'course description',
   //price: 100, imageLink: 'https://linktoimage.com', published: true }
@@ -57,6 +126,7 @@ app.post("/admin/courses", loginMiddleware, isAdmin, (req, res) => {
   if (course) {
     course.courseId = COURSES.length + 1;
     COURSES.push(course);
+    //writeData("COURSES.txt", COURSES);
   }
 
   res.status(200).json({
@@ -65,7 +135,7 @@ app.post("/admin/courses", loginMiddleware, isAdmin, (req, res) => {
   });
 });
 
-app.put("/admin/courses/:courseId", loginMiddleware, isAdmin, (req, res) => {
+app.put("/admin/courses/:courseId", loginMiddlewareAdmin, (req, res) => {
   //Body { title: 'updated course title', description: 'updated course description', price: 100, imageLink:
   //'https://updatedlinktoimage.com', published: false }
   //Output: { message: 'Course updated successfully' }
@@ -73,23 +143,29 @@ app.put("/admin/courses/:courseId", loginMiddleware, isAdmin, (req, res) => {
 
   let course = req.body;
   let courseId = req.params.courseId;
-
-  let courseIndex = COURSES.findIndex((course) => course.courseId === courseId);
+  //console.log("course " + JSON.stringify(course) + " course id " + courseId);
+  let courseIndex = COURSES.findIndex((course) => course.courseId == courseId);
 
   if (courseIndex === -1) {
     return res.status(404).send("Course not found");
   }
-  COURSES[courseIndex].title = course.title;
+
+  Object.assign(COURSES[courseIndex], course);
+  /* COURSES[courseIndex].title = course.title;
   COURSES[courseIndex].description = course.description;
+  COURSES[courseIndex].price = course.price;
   COURSES[courseIndex].imageLink = course.imageLink;
-  COURSES[courseIndex].published = course.published;
+  COURSES[courseIndex].published = course.published; */
 
   return res.status(200).json({ message: "Course updated successfully" });
 });
 
-app.get("/admin/courses", loginMiddleware, isAdmin, (req, res) => {
+app.get("/admin/courses", loginMiddlewareAdmin, (req, res) => {
   // logic to get all courses
-  return res.status(200).json(COURSES[courseIndex]);
+  /*   console.log("Admins " + JSON.stringify(ADMINS));
+  console.log("Users " + JSON.stringify(USERS));
+  console.log("Courses " + JSON.stringify(COURSES)); */
+  return res.status(200).json(COURSES);
 });
 
 // User routes
@@ -103,52 +179,54 @@ app.post("/users/signup", (req, res) => {
   }
 
   USERS.push({ username: username, password: password, purchasedCourses: [] });
+  //writeData("USERS.txt", USERS);
   return res.status(200).json({ message: "User created successfully" });
 });
 
-app.post("/users/login", loginMiddleware, (req, res) => {
+app.post("/users/login", loginMiddlewareUser, (req, res) => {
   // logic to log in user
   return res.status(200).json({ message: "User Logged in successfully" });
 });
 
-app.get("/users/courses", loginMiddleware, (req, res) => {
+app.get("/users/courses", loginMiddlewareUser, (req, res) => {
   // logic to list all courses
-  return res.status(200).json(COURSES);
+  return res.status(200).json(COURSES.filter((course) => course.published));
 });
 
-app.post("/users/courses/:courseId", loginMiddleware, (req, res) => {
+app.post("/users/courses/:courseId", loginMiddlewareUser, (req, res) => {
   // logic to purchase a course
-  let course = req.body;
+  //let course = req.body;
   let courseId = req.params.courseId;
 
-  let courseIndex = COURSES.findIndex((course) => course.courseId === courseId);
+  let courseIndex = COURSES.findIndex((course) => course.courseId == courseId);
 
   if (courseIndex === -1) {
     return res.status(404).send("Course not found");
   }
 
-  USERS[
-    USERS.findIndex((user) => user.username === req.headers[username])
-  ].purchasedCourses.push(COURSES[courseIndex]);
+  let userIndex = USERS.findIndex(
+    (user) => user.username === req.headers["username"]
+  );
 
+  USERS[userIndex].purchasedCourses.push(COURSES[courseIndex].courseId);
+  //console.log("USERS " + JSON.stringify(USERS[userIndex]));
+  //writeData("USERS.txt", USERS);
   return res.status(200).json({ message: "Course purchased successfully" });
 });
 
-app.get("/user/courses", loginMiddleware, isAdmin, (req, res) => {
-  // logic to get all courses
-  return res.status(200).json(COURSES[courseIndex]);
-});
-
-app.get("/users/purchasedCourses", loginMiddleware, (req, res) => {
+app.get("/users/purchasedCourses", loginMiddlewareUser, (req, res) => {
   // logic to view purchased courses
   return res
     .status(200)
     .json(
-      USERS[USERS.findIndex((user) => user.username === req.headers[username])]
-        .purchasedCourses
+      USERS[
+        USERS.findIndex((user) => user.username === req.headers["username"])
+      ].purchasedCourses.map((courseId) =>
+        COURSES.find((course) => course.courseId === courseId)
+      )
     );
 });
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  console.log("Server is listening on port: 3000");
 });
