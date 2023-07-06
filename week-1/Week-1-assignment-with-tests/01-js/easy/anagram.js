0d8eb744c088940b205419a7e64334924d476a65
Vinit Kumawat <vinitkumawat112@gmail.com>
2023-07-06 18:48:04 +0530

    added anagram code

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,12 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.split('').sort().join('');
+  str2 = str2.split('').sort().join('');
 
+  console.log(str1, str2);
+
+  return str1 === str2;
 }
 
 module.exports = isAnagram;
