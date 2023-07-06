31a417fca4da5371bc3affa15268a160f737bfb3
Mudit <muditert34@gmail.com>
2023-07-06 22:39:26 +0530

    02-medium-course-selling-app frontend

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./register.css";
 
-/// File is incomplete. You need to add input boxes to take input for users to register.
 function Register() {
-    const [email, setEmail] = React.useState("");
+  return (
+    <div className="container">
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
 
-    return <div>
-        <h1>Register to the website</h1>
-        <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        Already a user? <a href="/login">Login</a>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
     </div>
+  );
 }
 
 export default Register;
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import "./cardGallery.css";
 
-function ShowCourses() {
-    const [courses, setCourses] = React.useState([]);
+function CardGallery() {
+  const cards = [
+    {
+      id: 1,
+      image:
+        "https://www.classcentral.com/report/wp-content/uploads/2022/04/BCG-Web-Development-NEW-Banner.png",
+      title: "Card 1",
+      description: "Description 1",
+      price: "$10",
+    },
+    {
+      id: 2,
+      image:
+        "https://www.classcentral.com/report/wp-content/uploads/2022/04/BCG-Web-Development-NEW-Banner.png",
+      title: "Card 2",
+      description: "Description 2",
+      price: "$20",
+    },
+    {
+      id: 3,
+      image:
+        "https://www.classcentral.com/report/wp-content/uploads/2022/04/BCG-Web-Development-NEW-Banner.png",
+      title: "Card 3",
+      description: "Description 3",
+      price: "$30",
+    },
+  ];
 
-    // Add code to fetch courses from the server
-    // and set it in the courses state variable.
-    return <div>
-        <h1>Create Course Page</h1>
-        {courses.map(c => <Course title={c.title} />)}
+  const handleShowMore = (id) => {
+    // Handle logic to show more details for the card with the given id
+    console.log(`Show more for card with id ${id}`);
+  };
+
+  return (
+    <div className="card-gallery">
+      {cards.map((card) => (
+        <div className="card" key={card.id}>
+          <img src={card.image} alt={card.title} />
+          <h2>{card.title}</h2>
+          <p>{card.description}</p>
+          <span className="price">{card.price}</span>
+          <button onClick={() => handleShowMore(card.id)}>Show More</button>
+        </div>
+      ))}
     </div>
+  );
 }
 
-function Course(props) {
-    return <div>
-        <h1>{props.title}</h1>
-    </div>
-}
-
-export default ShowCourses;
+export default CardGallery;
