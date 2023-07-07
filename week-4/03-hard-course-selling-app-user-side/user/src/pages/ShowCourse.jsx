4026c8799ec8f780b103d6d49f3848967f6b1c26
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-08 03:46:20 +0530

    showing courses done

--- week-4/03-hard-course-selling-app-user-side/user/src/pages/ShowCourse.jsx
+++ week-4/03-hard-course-selling-app-user-side/user/src/pages/ShowCourse.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ShowCourse = () => {
-  return <div>ShowCourse</div>;
+  const [courses, setCourses] = useState();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const headers = new Headers();
+    headers.append("Authorization", `Bearer ${token}`);
+    fetch("http://localhost:3000/users/courses", {
+      method: "GET",
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then((data) => setCourses(data.courses));
+  }, []);
+  return (
+    <div>
+      <div>All Courses</div>
+      <br />
+      <br />
+      <div>
+        {courses &&
+          courses.map((course) => {
+            return (
+              <div key={course.id}>
+                <div>
+                  <h1>{course.title}</h1>
+                </div>
+                <div>
+                  <p>{course.description}</p>
+                </div>
+                <div>
+                  <h3>₹{course.price}</h3>
+                </div>
+                <br />
+                <br />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default ShowCourse;
