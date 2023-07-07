acba3f20302f085e554ec6da30f941542a48ff24
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-07 23:28:05 +0530

    medium todo done

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import { useState } from "react";
 /// You need to add input boxes to take input for users to create a course.
 /// I've added one input so you understand the api to do it.
 function CreateCourse() {
-    const [title, setTitle] = React.useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [isPublished, setIsPublished] = useState(false);
 
-    return <div>
-        <h1>Create Course Page</h1>
-        <input type={"text"} onChange={e => setTitle(e.target.value)} />
-        <button onClick={() => console.log(title)}>Create Course</button>
+  const handleOnClick = () => {
+    const courseDetails = {
+      title: title,
+      description: description,
+      price: price,
+      image: image,
+      published: isPublished,
+    };
+    const token = localStorage.getItem("token");
+    const headers = new Headers();
+    headers.append("Authorization", `Bearer ${token}`);
+
+    fetch("http://localhost:3000/admin/courses", {
+      method: "POST",
+      body: JSON.stringify(courseDetails),
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <h1>Create Course Page</h1>
+      <input type={"text"} onChange={(e) => setTitle(e.target.value)} />
+      <input type={"text"} onChange={(e) => setDescription(e.target.value)} />
+      <input type={"number"} onChange={(e) => setPrice(e.target.value)} />
+      <input type={"file"} onChange={(e) => setImage(e.target.value)} />
+      <input type="checkbox" onClick={() => setIsPublished(!isPublished)} />
+      <button onClick={handleOnClick}>Create Course</button>
     </div>
+  );
 }
 export default CreateCourse;
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,18 +1,59 @@
-import React from "react";
+import { useState } from "react";
 
 /// File is incomplete. You need to add input boxes to take input for users to login.
 function Login() {
-    const [email, setEmail] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return <div>
-        <h1>Login to admin dashboard</h1>
-        <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        <button>Login</button>
-        <br/>
-        New here? <a href="/register">Register</a>
+  const storeToken = (token) => {
+    const existingToken = localStorage.getItem("token");
+    if (existingToken) {
+      localStorage.removeItem("token");
+      localStorage.setItem("token", token);
+    }
+    localStorage.setItem("token", token);
+  };
+
+  const onHandleSignin = () => {
+    const data = {
+      email: email,
+      password: password,
+    };
+    const headers = new Headers();
+    headers.append("Credentials", JSON.stringify(data));
+    fetch("http://localhost:3000/admin/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: data,
+    })
+      .then((res) => res.json())
+      .then((data) => storeToken(data.token));
+  };
+
+  return (
+    <div>
+      <h1>Login to admin dashboard</h1>
+      <br />
+      Email -{" "}
+      <input
+        type={"text"}
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={onHandleSignin}>login</button>
+      <br />
+      New here? <a href="/register">Register</a>
     </div>
+  );
 }
 
 export default Login;
