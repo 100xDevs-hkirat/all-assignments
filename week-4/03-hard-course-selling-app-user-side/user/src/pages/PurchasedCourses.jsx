283ba09f8c973aa6fa2e3501d71296f43eb5d210
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-08 04:14:41 +0530

    all routes are implemented

--- week-4/03-hard-course-selling-app-user-side/user/src/pages/PurchasedCourses.jsx
+++ week-4/03-hard-course-selling-app-user-side/user/src/pages/PurchasedCourses.jsx
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const PurchesedCourses = () => {
-  return <div>PurchesedCourses</div>;
+  const [purchasedCourses, setPurchasedCourses] = useState();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const headers = new Headers();
+    headers.append("Authorization", `Bearer ${token}`);
+    fetch("http://localhost:3000/users/purchasedCourses", {
+      method: "GET",
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then((data) => setPurchasedCourses(data.purchasedCourses));
+  });
+  return (
+    <div>
+      <h2>PurchesedCourses</h2>
+      <div>
+        {purchasedCourses &&
+          purchasedCourses.map((course) => {
+            return (
+              <div key={course.id}>
+                <div>
+                  <h1>{course.title}</h1>
+                </div>
+                <div>
+                  <h3>{course.description}</h3>{" "}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default PurchesedCourses;
