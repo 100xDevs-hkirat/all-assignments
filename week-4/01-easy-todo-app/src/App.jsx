af1499a31838e3da2792704445a4930dfdaf9364
Ram123-hub <96813387+Ram123-hub@users.noreply.github.com>
2023-07-07 11:24:38 +0530

    Update App.jsx
    01-easy-todo-app

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -6,13 +6,90 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
     // fetch all todos from server
+  const [newTodo, setNewTodo] = useState("");
 
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+const fetchTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/todos");
+
+      if (response.ok) {
+        const data = await response.json();
+        setTodos(data);
+      } else {
+        console.error("Failed to fetch todos");
+      }
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
+
+const handleAddTodo = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: newTodo }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setTodos((prevTodos) => [...prevTodos, data]);
+        setNewTodo("");
+      } else {
+        console.error("Failed to add todo");
+      }
+    } catch (error) {
+      console.error("Error adding todo:", error);
+    }
+  };
+
+const handleDeleteTodo = async (index) => {
+    try {
+      const todoId = todos[index].id;
+
+      const response = await fetch(`http://localhost:3000/todos/${todoId}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        setTodos((prevTodos) => {
+          const updatedTodos = [...prevTodos];
+          updatedTodos.splice(index, 1);// Splice method is used to update, remove and delete elements in the array
+          return updatedTodos;
+        });
+      } else {
+        console.error("Failed to delete todo");
+      }
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
+  
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        <input type="text"
+           value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          />
+         <button onClick={handleAddTodo}>Add Todo</button>
       </div>
+        {todos.map((todo, index) => {
+        return (
+          <Todo
+            key={index}
+            title={todo.title}
+            onDelete={() => handleDeleteTodo(index)}
+          />
+        );
+      })}
     </>
   )
 }
@@ -21,6 +98,7 @@ function Todo(props) {
     // Add a delete button here so user can delete a TODO.
     return <div>
         {props.title}
+      <button onClick={props.onDelete}>Delete</button>
     </div>
 }
 
