c8211d1d19e87be8e5c907d780630c27062463f4
Nihar Hegde <niharhegde163@gmail.com>
2023-07-08 11:06:37 +0530

    assignment 1 isAnagram

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,23 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let upper1 = str1.toUpperCase();
+  let upper2 = str2.toUpperCase();
 
+  let split1 = upper1.split("");
+  let split2 = upper2.split("");
+
+  split1.sort();
+  split2.sort();
+
+  if (split1.join() == split2.join()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
