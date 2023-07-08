0bbd341a9b49cf0046b1f6e41a5bff167001f79b
krishna <krishnavm143@gmail.com>
2023-07-08 10:55:31 +0530

    completed 1st assignemrnt of week 4

--- week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
+++ week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require("fs");
+const fs = require('fs');
+
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
@@ -22,14 +24,14 @@ function removeAtIndex(arr, index) {
 }
 
 app.get('/todos', (req, res) => {
-  fs.readFile("todos.json", "utf8", (err, data) => {
+  fs.readFile('todos.json', 'utf8', (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
   });
 });
 
 app.get('/todos/:id', (req, res) => {
-  fs.readFile("todos.json", "utf8", (err, data) => {
+  fs.readFile('todos.json', 'utf8', (err, data) => {
     if (err) throw err;
     const todos = JSON.parse(data);
     const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -45,13 +47,13 @@ app.post('/todos', (req, res) => {
   const newTodo = {
     id: Math.floor(Math.random() * 1000000), // unique random id
     title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
   };
-  fs.readFile("todos.json", "utf8", (err, data) => {
+  fs.readFile('todos.json', 'utf8', (err, data) => {
     if (err) throw err;
     const todos = JSON.parse(data);
     todos.push(newTodo);
-    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+    fs.writeFile('todos.json', JSON.stringify(todos), (err) => {
       if (err) throw err;
       res.status(201).json(newTodo);
     });
@@ -59,7 +61,7 @@ app.post('/todos', (req, res) => {
 });
 
 app.put('/todos/:id', (req, res) => {
-  fs.readFile("todos.json", "utf8", (err, data) => {
+  fs.readFile('todos.json', 'utf8', (err, data) => {
     if (err) throw err;
     const todos = JSON.parse(data);
     const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -69,10 +71,10 @@ app.put('/todos/:id', (req, res) => {
       const updatedTodo = {
         id: todos[todoIndex].id,
         title: req.body.title,
-        description: req.body.description
+        description: req.body.description,
       };
       todos[todoIndex] = updatedTodo;
-      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      fs.writeFile('todos.json', JSON.stringify(todos), (err) => {
         if (err) throw err;
         res.status(200).json(updatedTodo);
       });
@@ -81,8 +83,7 @@ app.put('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-
-  fs.readFile("todos.json", "utf8", (err, data) => {
+  fs.readFile('todos.json', 'utf8', (err, data) => {
     if (err) throw err;
     const todos = JSON.parse(data);
     const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -90,7 +91,7 @@ app.delete('/todos/:id', (req, res) => {
       res.status(404).send();
     } else {
       todos = removeAtIndex(todos, todoIndex);
-      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      fs.writeFile('todos.json', JSON.stringify(todos), (err) => {
         if (err) throw err;
         res.status(200).send();
       });
@@ -101,6 +102,10 @@ app.delete('/todos/:id', (req, res) => {
 // for all other routes, return 404
 app.use((req, res, next) => {
   res.status(404).send();
+});
+
+app.listen(port, () => {
+  console.log(`App running at ${port}`);
 });
 
 module.exports = app;
--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import TodoList from '../component/TotdoList'
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [error, setError] = useState(null)
   const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  const [fetchAll, setFetchAll] = useState(false)
 
+  // fetch all todos from server
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const _fetchtodo = await fetch(`http://localhost:4000/todos`);
+        const res = await _fetchtodo.json();
+        console.log(res.length);
+        if (res.length === 0) {
+          throw new Error('No data available.');
+        } else {
+          setTodos([...res]);
+        }
+      } catch (error) {
+        console.error(error);
+        setTodos([])
+        // Handle the error appropriately, e.g., set an error state or display an error message.
+      }
+    };
+    fetchTodo();
+  }, [fetchAll]);
+
+
+  const submitBtnClick = async (e) => {
+    e.preventDefault()
+    if (title.length === 0 && description.length === 0) {
+      setError('title or descitpion should not be empty')
+      return
+    }
+    console.log('title', title)
+    console.log('description', description)
+    try {
+      const response = await fetch(`http://localhost:4000/todos`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title, description })
+      },)
+      if (!response.ok) {
+        throw new Error('Something wen wrong')
+      }
+      const data = await response.json()
+      console.log(data)
+      setFetchAll((fetch) => !fetch)
+      setTitle('')
+      setDescription('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const deletebtn = async (id) => {
+    console.log(id)
+    try {
+      const response = await fetch(`http://localhost:4000/todos/${id}`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'Application/json'
+        }
+      }
+      )
+      if (response.ok) {
+        console.log('deleted successfully')
+        setFetchAll(fetch => !fetch)
+      } else {
+        throw new Error('Something went wrong')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
   return (
     <>
-      <div>
+      <div className='todo-container'>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        <form onSubmit={submitBtnClick}>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" onChange={(e) => (
+              setError(''),
+              setTitle(e.target.value)
+            )} value={title} name='title' id='title' />
+            <p style={{ color: 'red' }}>{error}</p>
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <input type="text" onChange={(e) => (setError(''), setDescription(e.target.value))} value={description} name="description" id="description" />
+            <p style={{ color: 'red' }}>{error}</p>
+          </div>
+          <div><button>Submit</button></div>
+        </form>
+      </div>
+      <div>
+        {
+          todos.length === 0 ? (<div>No Data Available</div>) :
+            (todos.map(todo => (
+              <TodoList key={todo.id} id={todo.id} title={todo.title} description={todo.description} onDelete={deletebtn} />
+            )))
+        }
       </div>
     </>
   )
 }
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
-}
 
 export default App
