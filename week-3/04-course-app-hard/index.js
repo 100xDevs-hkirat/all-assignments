b28bf044defb68f0f19d613438d3dea2e2222a22
Anjan Talatam <tspa2117@gmail.com>
2023-07-09 11:53:43 +0530

    👌 IMPROVE: fix cors, add adminMiddleware

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const lodash = require('lodash');
+
 const app = express();
 const PORT = 3000;
 
 const jwtSecret = 'dev-jwt-secret';
 const { Admin, Course, User } = require('./models');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose
@@ -34,13 +38,32 @@ const COURSE_RES_PROPS = [...COURSE_PROPS, 'id'];
 function authenticateJwt(req, res, next) {
   const { authorization } = req.headers;
 
+  if (!authorization) {
+    return res.status(401).send({
+      message: 'Missing Authorization Headers',
+      code: 'token_missing',
+    });
+  }
+
   try {
     const decryptedJwt = jwt.verify(authorization, jwtSecret);
     req.user = decryptedJwt;
     next();
   } catch (e) {
-    return res.status(401).send({ message: 'Token expired' });
+    return res
+      .status(401)
+      .send({ message: 'Token expired', code: 'token_expired' });
   }
+}
+
+function adminMiddleware(req, res, next) {
+  const { role } = req.user;
+
+  if (role !== 'admin') {
+    return res.status(403).send({ message: 'Access restricted' });
+  }
+
+  next();
 }
 
 // Admin routes
@@ -68,7 +91,9 @@ app.post('/admin/signup', async (req, res) => {
 
   await newAdmin.save();
 
-  res.status(200).send({ message: 'Admin created successfully', token });
+  res
+    .status(200)
+    .send({ message: 'Admin created successfully', token, role: 'admin' });
 });
 
 app.post('/admin/login', async (req, res) => {
@@ -91,75 +116,97 @@ app.post('/admin/login', async (req, res) => {
     expiresIn: '1h', // expires in 1hour
   });
 
-  res.send({ message: 'Logged in successfully', token });
+  res.send({ message: 'Logged in successfully', token, role: 'admin' });
 });
 
-app.post('/admin/courses', authenticateJwt, async (req, res) => {
-  const { username } = req.user;
+app.post(
+  '/admin/courses',
+  authenticateJwt,
+  adminMiddleware,
+  async (req, res) => {
+    const { username } = req.user;
 
-  const newCourse = new Course(req.body);
+    const newCourse = new Course(req.body);
 
-  const adminFromDb = await Admin.findOne({ username });
+    const adminFromDb = await Admin.findOne({ username });
 
-  newCourse.instructorId = adminFromDb._id;
+    newCourse.instructorId = adminFromDb._id;
 
-  try {
-    const createdCourse = await newCourse.save();
-    res.send({
-      message: 'Course created successfully',
-      courseId: createdCourse._id,
-    });
-  } catch (e) {
-    res.status(401).send({ message: 'All properties are required' });
-  }
-});
-
-app.put('/admin/courses/:courseId', authenticateJwt, async (req, res) => {
-  const { courseId } = req.params;
-  const body = req.body;
-
-  if (COURSE_PROPS.every((prop) => body[prop] === undefined)) {
-    return res.status(400).send({ message: 'Atleast one field required' });
-  }
-
-  const updatedCourse = {};
-
-  // this ensures only title, description, .. are only updated
-  COURSE_PROPS.forEach((prop) => {
-    if (body[prop] !== undefined) {
-      updatedCourse[prop] = body[prop];
+    try {
+      const createdCourse = await newCourse.save();
+      res.send({
+        message: 'Course created successfully',
+        courseId: createdCourse._id,
+      });
+    } catch (e) {
+      res.status(401).send({ message: 'All properties are required' });
     }
-  });
+  }
+);
 
-  try {
-    const newUpdatedCourseFromDb = await Course.findByIdAndUpdate(
-      courseId,
-      updatedCourse,
-      {
-        new: true,
+app.put(
+  '/admin/courses/:courseId',
+  authenticateJwt,
+  adminMiddleware,
+  async (req, res) => {
+    const { courseId } = req.params;
+    const body = req.body;
+
+    if (COURSE_PROPS.every((prop) => body[prop] === undefined)) {
+      return res.status(400).send({ message: 'Atleast one field required' });
+    }
+
+    const updatedCourse = {};
+
+    // this ensures only title, description, .. are only updated
+    COURSE_PROPS.forEach((prop) => {
+      if (body[prop] !== undefined) {
+        updatedCourse[prop] = body[prop];
       }
-    );
+    });
 
-    if (!newUpdatedCourseFromDb) {
-      return res.status(401).send({ message: 'Course not found' });
+    try {
+      const newUpdatedCourseFromDb = await Course.findByIdAndUpdate(
+        courseId,
+        updatedCourse,
+        {
+          new: true,
+        }
+      );
+
+      if (!newUpdatedCourseFromDb) {
+        return res.status(401).send({ message: 'Course not found' });
+      }
+
+      res.send({ message: 'Course updated successfully' });
+    } catch (e) {
+      console.log(e, 'catch error: update course');
+      return res.status(500).send({ message: 'Something went wrong' });
     }
-
-    res.send({ message: 'Course updated successfully' });
-  } catch (e) {
-    console.log(e, 'catch error: update course');
-    return res.status(500).send({ message: 'Something went wrong' });
   }
-});
+);
 
-app.get('/admin/courses', authenticateJwt, async (req, res) => {
-  const { username } = req.user;
+app.get(
+  '/admin/courses',
+  authenticateJwt,
+  adminMiddleware,
+  async (req, res) => {
+    const { username } = req.user;
 
-  const adminFromDb = await Admin.findOne({ username });
+    const adminFromDb = await Admin.findOne({ username });
 
-  const coursesByAdmin = await Course.find({ instructorId: adminFromDb._id });
+    // if(!adminFromDb)
 
-  return res.send({ courses: coursesByAdmin });
-});
+    const coursesByAdmin = await Course.find({ instructorId: adminFromDb._id });
+
+    const coursesResponse = coursesByAdmin.map((course) => ({
+      id: course._id,
+      ...lodash.pick(course, COURSE_PROPS),
+    }));
+
+    return res.send({ courses: coursesResponse });
+  }
+);
 
 // <-------------------- Admin DEV Route -------------------->
 app.get('/admins', async (req, res) => {
@@ -199,7 +246,9 @@ app.post('/users/signup', async (req, res) => {
     expiresIn: '1h',
   });
 
-  res.status(200).send({ message: 'User created successfully', token });
+  res
+    .status(200)
+    .send({ message: 'User created successfully', token, role: 'user' });
 });
 
 app.post('/users/login', async (req, res) => {
@@ -222,7 +271,7 @@ app.post('/users/login', async (req, res) => {
     expiresIn: '1h',
   });
 
-  res.send({ message: 'Logged in successfully', token });
+  res.send({ message: 'Logged in successfully', token, role: 'user' });
 });
 
 app.get('/users/courses', authenticateJwt, async (req, res) => {
