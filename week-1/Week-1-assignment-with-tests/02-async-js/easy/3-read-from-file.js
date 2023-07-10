2a1c21e2be8d0cc5d5f4ea9871e87026ed3b3ee4
Durgesh Nishad <dnishad042@gmail.com>
2023-07-10 19:19:37 +0530

    solved 3-read-from-file.js

---
+++ week-1/Week-1-assignment-with-tests/02-async-js/easy/3-read-from-file.js
@@ -0,0 +1,18 @@
+/* the contents of a file
+
+Write code to read contents of a file and print it to the console. 
+You can use the fs library to as a black box, the goal is to understand async tasks. 
+Try to do an expensive operation below the file read and see how it affects the output. 
+Make the expensive operation more and more expensive and see how it affects the output. */
+
+  const fs= require('fs');
+
+  function printFile(err, data){
+    if(err){
+        comsole.log(err);
+        return;
+    }
+    console.log(data);
+  }
+
+  fs.readFile('readme.txt', 'utf8', printFile)
