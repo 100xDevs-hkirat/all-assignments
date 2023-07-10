d9f7a5b6b63f4b48bd43c107375850e9a49c3605
haki-user <adityapratapsingh33@outlook.com>
2023-07-10 22:22:42 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	new file:   .gitignore
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/vite.svg
    	new file:   src/App.css
    	new file:   src/App.jsx
    	new file:   src/assets/react.svg
    	new file:   src/components/Courses.jsx
    	new file:   src/components/Landing.jsx
    	new file:   src/components/Login.jsx
    	new file:   src/components/PurchaseCourse.jsx
    	new file:   src/components/PurchasedCourses.jsx
    	new file:   src/components/Register.jsx
    	new file:   src/index.css
    	new file:   src/main.jsx
    	new file:   vite.config.js
    
     Changes not staged for commit:
    	modified:   ../../../week-3/solutions/03-course-app-medium.js
    	modified:   ../../../week-3/solutions/courses.json
    	modified:   ../../../week-3/solutions/users.json
    	modified:   ../../03-hard-course-selling-app-user-side/vite-project/src/components/Landing.jsx
    	modified:   ../../03-hard-course-selling-app-user-side/vite-project/src/components/PurchaseCourse.jsx
    	modified:   ../../03-hard-course-selling-app-user-side/vite-project/src/components/PurchasedCourses.jsx

---
+++ week-4/04-hard-use-mui/with-mui/src/components/Register.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Register() {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const navigate = useNavigate();
+
+    function register(event) {
+        event.preventDefault();
+        axios.post('http://localhost:3000/users/signup', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            username: email,
+            password,
+        }).then(res => {
+            if(res.data.token) {
+                localStorage.setItem('token', res.data.token);
+                alert(res.data.message);
+                navigate('/');
+            }
+        })
+        .catch(err => console.error(err));
+    }
+
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <br />
+            <form onSubmit={register}>
+                Email - <input type="email" onChange={e => setEmail(e.target.value)} />
+                <br />
+                Password - <input type="password" onChange={e => setPassword(e.target.value)} />
+                <br />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
---
+++ week-4/04-hard-use-mui/with-mui/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
