a3f4de513958509201f6fb38169bbeec60130a21
SlokAks <slok.aks204@gmail.com>
2023-07-13 13:04:13 +0530

    implemented week-4/02-medium-course-selling-app-admin-dashboard

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,18 +1,61 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-/// File is incomplete. You need to add input boxes to take input for users to login.
+const adminLoginURL = "http://localhost:3000/admin/login";
+
 function Login() {
-    const [email, setEmail] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
 
-    return <div>
-        <h1>Login to admin dashboard</h1>
-        <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        <button>Login</button>
-        <br/>
-        New here? <a href="/register">Register</a>
+  const handleLogin = () => {
+    fetch(adminLoginURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res);
+      })
+      .then((data) => {
+        let token = data.token;
+        let message = data.message;
+
+        alert(`${message}`);
+        if (token != undefined) {
+          localStorage.setItem("token", token);
+          navigate("/about");
+        }
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login to admin dashboard</h1>
+      <br />
+      Email <input type={"text"} onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      Password{" "}
+      <input type={"text"} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <br />
+      <button onClick={handleLogin}>Login</button>
+      <br />
+      <br />
+      New here? <a href="/register">Register</a>
     </div>
+  );
 }
 
 export default Login;
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 
 function ShowCourses() {
-    const [courses, setCourses] = React.useState([]);
+  const [courses, setCourses] = React.useState([]);
+  const [count, setCount] = React.useState(0)
 
-    // Add code to fetch courses from the server
-    // and set it in the courses state variable.
-    return <div>
-        <h1>Create Course Page</h1>
-        {courses.map(c => <Course title={c.title} />)}
+  // Add code to fetch courses from the server
+  // and set it in the courses state variable.
+  React.useEffect(() => {
+    fetch("http://localhost:3000/admin/courses", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem('token')}`,
+      }
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(res);
+    })
+    .then((data) => {
+      console.log(data.courses);
+      setCourses(data.courses)
+    });
+  }, [count])
+  
+  return (
+    <div>
+      <h1>Create Course Page</h1>
+      {courses.map((course) => <Course key={course.id} id={course.id} title={course.title} />)}
     </div>
+  );
 }
 
 function Course(props) {
-    return <div>
-        <h1>{props.title}</h1>
+  return (
+    <div>
+      <span><h3>{props.id} {props.title}</h3></span>
     </div>
+  );
 }
 
 export default ShowCourses;
