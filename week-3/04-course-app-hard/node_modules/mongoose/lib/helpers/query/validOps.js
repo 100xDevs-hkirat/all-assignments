bda318a387b840a12080adc02cac21e0968d8a8c
Harsh <harshchauhan6176@gmail.com>
2023-07-13 15:30:14 +0530

    Added mongoDB to store data

---
+++ week-3/04-course-app-hard/node_modules/mongoose/lib/helpers/query/validOps.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = Object.freeze([
+  // Read
+  'count',
+  'countDocuments',
+  'distinct',
+  'estimatedDocumentCount',
+  'find',
+  'findOne',
+  // Update
+  'findOneAndReplace',
+  'findOneAndUpdate',
+  'replaceOne',
+  'updateMany',
+  'updateOne',
+  // Delete
+  'deleteMany',
+  'deleteOne',
+  'findOneAndDelete',
+  'findOneAndRemove'
+]);
