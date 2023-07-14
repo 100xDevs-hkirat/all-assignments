7266a899d587737bed385c1cf45015f3aaddd417
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-07-14 02:40:07 -0700

    code: get todos done

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function useTodo() {
   const [todos, setTodos] = useState([])
 
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:3000/todos", {
+      method: 'GET' 
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log(data)
+        setTodos(data);
+      });
+    });
+    }, 1000)
+
+  }, []);
+  return todos;
+  
 }
 
 function App() {
   
     // fetch all todos from server
+    const todo = useTodo();
 
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
         <input type="text" />
+      </div>
+      <div>
+        {todo.map(todo => {
+          return <Todo title = {todo.title} description = {todo.description}></Todo>
+        })}
       </div>
     </>
   )
@@ -24,6 +45,7 @@ function Todo(props) {
     // Add a delete button here so user can delete a TODO.
     return <div>
         {props.title}
+        {props.description}
     </div>
 }
 
