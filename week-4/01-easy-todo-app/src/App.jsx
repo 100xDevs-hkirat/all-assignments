6f241da987104cae6544983c3a6115e0027f14be
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-07-14 02:18:58 -0700

    code: started todo server

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function useTodo() {
   const [todos, setTodos] = useState([])
+
+}
+
+function App() {
+  
     // fetch all todos from server
 
   return (
