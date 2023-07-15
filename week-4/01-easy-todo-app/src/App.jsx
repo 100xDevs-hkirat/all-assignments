1e438064911c3e34efa2a36456c5a7bd7ab07db9
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2023-07-15 19:34:53 +0530

    retodo app frontend

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+  // fetch all todos from server
+  const fetchTodo = () => {
+    fetch("http://localhost:3000/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  };
+  useEffect(() => {
+    fetchTodo();
+  }, []);
 
+  const handleAdd = () => {
+    const todo = inputRef.current.value;
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: todo, description: todo }),
+    }).then((res) => {
+      alert("todo added successfully!!!");
+      inputRef.current.value = "";
+      fetchTodo();
+    });
+  };
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        <input type="text" ref={inputRef} />{" "}
+        <button onClick={() => handleAdd()}>add</button>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} fetchTodo={fetchTodo} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
+function Todo({ todo, fetchTodo }) {
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`http://localhost:3000/todos/${id}`, { method: "DELETE" }).then(
+      () => {
+        fetchTodo();
+      }
+    );
+  };
+  // Add a delete button here so user can delete a TODO.
+  return (
+    <div>
+      {todo.title}. {todo.description}{" "}
+      <button onClick={() => handleDelete(todo.id)}>delete</button>
     </div>
+  );
 }
 
-export default App
+export default App;
