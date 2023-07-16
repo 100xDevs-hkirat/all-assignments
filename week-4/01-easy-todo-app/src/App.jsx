daca25e1449f886a67737c176d1a0212351c135c
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-07-16 14:45:52 -0700

    code: added todo api and delete button

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,52 +1,48 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
-function useTodo() {
-  const [todos, setTodos] = useState([])
-
-  useEffect(() => {
-    setInterval(() => {
-      fetch("http://localhost:3000/todos", {
-      method: 'GET' 
-    }).then((response) => {
-      response.json().then((data) => {
-        console.log(data)
-        setTodos(data);
-      });
-    });
-    }, 1000)
-
-  }, []);
-  return todos;
-  
-}
 
 function App() {
-  
     // fetch all todos from server
-    const todo = useTodo();
+    const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/todos").then((res) => {
+      setTodos(res.data);
+    })
+  }, []);
+
+  const deleteTodo = (id) => {
+    axios.delete(`http://localhost:3000/todos/${id}`).then(res => setTodos(res.data));
+  }
 
   return (
     <>
       <div>
-        <h1>Easy Todo App</h1>
+        <h1>Todo App</h1>
+        <label>Enter a todo: </label>
         <input type="text" />
       </div>
-      <div>
-        {todo.map(todo => {
-          return <Todo title = {todo.title} description = {todo.description}></Todo>
-        })}
-      </div>
+      <ol>
+        {todos.map(todo => (
+          <ShowTodo key={todo.id} todo={todo} deleteTodo={deleteTodo}/>
+        ))}
+      </ol>
     </>
   )
 }
 
-function Todo(props) {
+function ShowTodo({ todo, deleteTodo}) {
     // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-        {props.description}
-    </div>
+    const {id, title } = todo;
+    return (
+      <li>
+        <b>Title:</b> {title} <br />
+        <button onClick={() => deleteTodo(id)}>Delete</button>
+        <hr />
+      </li>
+    );
 }
 
 export default App
