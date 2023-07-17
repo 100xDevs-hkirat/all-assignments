616f815120a14577eb591a953dfaa40ee80d223a
Aftab Naik <aftabnaik1419@gmail.com>
2023-07-17 17:57:24 +0530

    Solved medium assignment palindrome

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -1,3 +1,5 @@
+const isAnagram = require("../easy/anagram");
+
 /*
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
@@ -5,9 +7,23 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
-
-function isPalindrome(str) {
-  return true;
+function ReverseString(str) {
+  return str.split("").reverse().join("");
+}
+function removeSpacesAndPunctuation(text) {
+  return text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
 }
 
+function isPalindrome(str) {
+  // let str2 = convertToLowerCase(str);
+  str = removeSpacesAndPunctuation(str);
+  str = str.toLowerCase();
+
+  let str2 = ReverseString(str);
+  if (str == str2) return true;
+  else return false;
+}
+str = "Eva, can I see bees in a cave?";
+// var str = str.toLowerCase();
+console.log( isPalindrome(str) );
 module.exports = isPalindrome;
