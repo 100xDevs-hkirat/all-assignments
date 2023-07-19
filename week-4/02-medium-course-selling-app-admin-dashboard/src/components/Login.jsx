10a72072fa4a2813ea89dd5c978be291e2874647
Akash Gahlot <akashgahlot@Akashs-MacBook-Pro.local>
2023-07-19 16:48:13 +0530

    react course app

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import axios from "axios";
 
 /// File is incomplete. You need to add input boxes to take input for users to login.
 function Login() {
     const [email, setEmail] = React.useState("");
+    const [password, setpassword]=React.useState("");
+    
+    function login(){
+        const url="http://localhost:3000/admin/login";
+        const data={
+            email:email,
+            password:password,
+        }        
+        axios.post(url,data).then((response)=>{
+            console.log(response.data);
+            console.log(response.header.bearerToken);
+            const bearerToken = response.headers.authorization;
+            console.log('Bearer Token:', bearerToken);
+        
+        }).catch((err)=>{
+            console.log("Error : "+err);
+        })
+    }
 
     return <div>
         <h1>Login to admin dashboard</h1>
         <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
+        Email - <input type={"text"} value ={email} onChange={e => setEmail(e.target.value)} />
         <br/>
-        <button>Login</button>
+        <br/>
+        Password-<input type="password" value={password} onChange={e=>setpassword(e.target.value)}/>
+        <br/>
+        <button onClick={login}>Login</button>
         <br/>
         New here? <a href="/register">Register</a>
     </div>
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -1,15 +1,40 @@
+import axios from "axios";
 import React from "react";
 
 /// File is incomplete. You need to add input boxes to take input for users to register.
 function Register() {
     const [email, setEmail] = React.useState("");
-
+    const [username,setusername]=React.useState("");
+    const [password,setpassword]=React.useState("");    
+    function register(){
+        const url="http://localhost:3000/admin/signup";
+        const data={
+            username:username,
+            password:password,
+            email:email,
+        }       
+        axios.post(url,data).then((response)=>{
+            
+                console.log(response.data);
+                const bearerToken = response.headers.authorization;
+                console.log('Bearer Token:', bearerToken);
+            
+        }).catch((err)=>{
+            console.log("Error : "+err);
+        })
+    }
     return <div>
         <h1>Register to the website</h1>
         <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
+        Email - <input type={"email"} value ={email} onChange={e => setEmail(e.target.value)} />
         <br/>
-        Already a user? <a href="/login">Login</a>
+        UserName -<input type={"text"} value={username} onChange={e=>setusername(e.target.value)}/>
+        <br/>
+        Password-<input type={"password"} value={password} onChange={e=>setpassword(e.target.value)}/>
+        <br/>
+        <button onClick={register}>Register</button>
+        <br/>
+        Already a user? <a href="/login" >Login</a>
     </div>
 }
 
