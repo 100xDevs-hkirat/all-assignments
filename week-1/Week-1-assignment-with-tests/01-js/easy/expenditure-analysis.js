9fbb5dee3d0a36ce19a8f34d6f23a2db76b479d9
Aftab Naik <aftabnaik1419@gmail.com>
2023-07-24 18:35:04 +0530

    Solved expenditure analysis easy

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -9,7 +9,25 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = new Map();
+
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+    if (categoryMap.has(category)) {
+      categoryMap.set(category, categoryMap.get(category) + price);
+    } else {
+      categoryMap.set(category, price);
+    }
+  });
+
+  const result = [];
+
+  for (let [category, totalSpent] of categoryMap) {
+    const obj = { category, totalSpent };
+    result.push(obj);
+  }
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
