5136cfd9b97a6f7641b75e6385c656eec87bb89b
GourabSanyal <sanyal.gourab@gmail.com>
2023-07-25 16:20:40 +0530

    admin login logic

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
 app.use(express.json());
 
@@ -9,60 +9,71 @@ let USERS = [];
 let COURSES = [];
 
 app.get("/", (req, res) => {
-  res.json({ message : "Home route"})
-})
+  res.json({ message: "Home route" });
+});
 
-const adminAuthentication = () => {
+const adminAuthentication = (req, res, next) => {
+  const { username, password } = req.body;
 
-}
+  const admin = ADMINS.find(
+    (a) => (a.username === username) & (a.password === password)
+  );
+
+  if (admin) {
+    next()
+  } else (
+    res.status(403).json({ message : "Admin authentication has failed"})
+  )
+};
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
-  const admin = req.body
-  const existingAdmin = ADMINS.find(a => a.username === admin.username)
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
 
-  if ( existingAdmin){
-    res.status(403).json({message : "Admin already exists"})
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
   } else {
-    ADMINS.push(admin)
-    res.json({message : "Admin signup successful"})
+    ADMINS.push(admin);
+    res.json({ message: "Admin signup successful" });
   }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post("/admin/login", adminAuthentication, (req, res) => {
   // logic to log in admin
+  res.json({ message : "Admin logged in successfully"})
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post("/admin/courses", (req, res) => {
   // logic to create a course
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", (req, res) => {
   // logic to edit a course
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", (req, res) => {
   // logic to get all courses
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", (req, res) => {
   // logic to sign up user
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", (req, res) => {
   // logic to log in user
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", (req, res) => {
   // logic to list all courses
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", (req, res) => {
   // logic to purchase a course
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", (req, res) => {
   // logic to view purchased courses
 });
 
