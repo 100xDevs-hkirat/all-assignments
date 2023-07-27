b6df78268a6e3bcc69be43db2ae950c1b2ba711f
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-07-27 10:38:15 -0700

    code: installed recoil

--- week-4/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -7,6 +7,7 @@ import ShowCourses from './components/ShowCourses';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
 import Header from './sub-components/Header';
 import Course from './components/Course';
+import { RecoilRoot } from 'recoil';
 
 // This file shows how you can do routing in React.
 // Try going to /login, /register, /about, /courses on the website and see how the html changes
@@ -22,17 +23,19 @@ const theme = createTheme({
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Router>
-            <Header/>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/createcourse" element={<CreateCourse />} />
-                    <Route path="/courses" element={<ShowCourses />} />
-                    <Route path="/course/:courseId" element={<Course />} />
-                </Routes>
-            </Router>
+            <RecoilRoot>
+                <Router>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/createcourse" element={<CreateCourse />} />
+                        <Route path="/courses" element={<ShowCourses />} />
+                        <Route path="/course/:courseId" element={<Course />} />
+                    </Routes>
+                </Router>
+            </RecoilRoot>
         </ThemeProvider>
     );
 }
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import React, { useEffect } from "react";
 import { Card, Grid, Box, CardContent, CardMedia, Typography, Button } from '@mui/material';
