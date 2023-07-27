0c6a6203573f009d0de87da1b22df8bf428ca247
asbar <asbar03KK@gmail.com>
2023-07-27 23:39:08 +0530

    resolved coflict

--- week-7/assignment-1/index.js
+++ week-7/assignment-1/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const todoRouter = require("./routes/todo");
-const authRouter = require("./routes/auth");
+const mongoose = require("mongoose");
+const port = 3000;
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: "todos",
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.use("/todo", todoRouter);
-app.use("/auth", authRouter);
-
-app.listen(3000);
+mongoose.connect("mongodb://localhost:27017/courses", { dbName: "courses" });
