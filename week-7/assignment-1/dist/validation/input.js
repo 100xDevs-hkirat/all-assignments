f10c0cd3efa097caa72e544e573514ff3b9c1e04
ThFate <kunaalrw2004@gmail.com>
2023-07-28 13:18:55 +0530

    removed dist

--- week-7/assignment-1/dist/validation/input.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Password = exports.Username = void 0;
-const zod_1 = require("zod");
-// does not match username containing numbers
-const usernameRegex = new RegExp('^([^0-9]*)$');
-const passwordRegex = new RegExp('[A-Z]');
-exports.Username = zod_1.z.string().min(4).max(12).regex(usernameRegex);
-exports.Password = zod_1.z.string().min(8).max(22).regex(passwordRegex);
