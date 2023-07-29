6372461a2c9ad9dd9307dc23e88cf7fee77c9467
Akinventery <126159211+Akinventery@users.noreply.github.com>
2023-07-29 21:36:21 +0530

    Update anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -7,8 +7,29 @@
   - `npm run test-anagram`
 */
 
+// function to sort string 
+function sortString(str){
+  // first convert string to array 
+  strArry = str.split("");
+  // now sort the array
+  strArry = strArry.sort();
+  // again join array to get string
+  strNew = strArry.join("");
+
+  return strNew;
+}
+
 function isAnagram(str1, str2) {
 
+  if(sortString(str1) != sortString(str2))
+     return false;
+  
+  return true;
+  
 }
+
+
+console.log(isAnagram('ak','ka'));
+
 
 module.exports = isAnagram;
