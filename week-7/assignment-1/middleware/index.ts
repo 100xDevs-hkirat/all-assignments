df89df4c496ae1fbdb93e740fe0e459cc7964de0
Sanjay <sanjay@pop-os.localdomain>
2023-08-05 02:05:03 +0530

    completed Assignment 1

---
+++ week-7/assignment-1/middleware/index.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request , Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const SECRET = 'SECr3t';  // This should be in an environment variable in a real application
+
+export const authenticateJwt = (req: Request, res: Response, next:NextFunction ) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      if(!user){
+        return res.sendStatus(403);
+      }
+    if(typeof user === "string"){
+        return res.sendStatus(403);
+      }
+      req.headers["userId"] = user.id;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
--- week-7/assignment-1/routes/todo.ts
+++ week-7/assignment-1/routes/todo.ts
@@ -1,14 +1,20 @@
-const express = require('express');
-const { authenticateJwt, SECRET } = require("../middleware/index");
-const { Todo } = require("../db");
+import express from 'express';
+import { authenticateJwt, SECRET } from "../middleware/index";
+import { Todo } from "../db";
 const router = express.Router();
 
-router.post('/todos', authenticateJwt, (req, res) => {
-  const { title, description } = req.body;
-  const done = false;
-  const userId = req.userId;
+interface createToDoInput {
+  title: string;
+  description: string;
+}
 
-  const newTodo = new Todo({ title, description, done, userId });
+
+router.post('/todos', authenticateJwt, (req, res) => {
+  const input: createToDoInput = req.body;
+  const done = false;
+  const userId = req.headers["userId"];
+
+  const newTodo = new Todo({ title: input.title , description: input.description , done, userId });
 
   newTodo.save()
     .then((savedTodo) => {
@@ -21,7 +27,8 @@ router.post('/todos', authenticateJwt, (req, res) => {
 
 
 router.get('/todos', authenticateJwt, (req, res) => {
-  const userId = req.userId;
+  const userId = req.headers["userId"];
+
 
   Todo.find({ userId })
     .then((todos) => {
@@ -34,7 +41,7 @@ router.get('/todos', authenticateJwt, (req, res) => {
 
 router.patch('/todos/:todoId/done', authenticateJwt, (req, res) => {
   const { todoId } = req.params;
-  const userId = req.userId;
+  const userId = req.headers["userId"];
 
   Todo.findOneAndUpdate({ _id: todoId, userId }, { done: true }, { new: true })
     .then((updatedTodo) => {
@@ -48,4 +55,4 @@ router.patch('/todos/:todoId/done', authenticateJwt, (req, res) => {
     });
 });
 
-module.exports = router;
+export default router;
