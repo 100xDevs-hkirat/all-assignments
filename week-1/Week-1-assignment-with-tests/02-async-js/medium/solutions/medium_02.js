9103586ef04d35ddd67e4be694f6d2da76cdcf28
codescalper <msfunbook@gmail.com>
2023-08-04 17:45:12 +0530

    Done with medium async questions

---
+++ week-1/Week-1-assignment-with-tests/02-async-js/medium/solutions/medium_02.js
@@ -0,0 +1,9 @@
+
+
+setInterval(function(){
+    const time = new Date();
+    // console.log(`${time.getHours()}:${time.getMinutes()}::${time.getSeconds()}`)
+    console.log(time.toLocaleTimeString())
+
+},1000)
+
