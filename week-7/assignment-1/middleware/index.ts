b7a4820b4a74d7a532c389170a643d6e8339ec3b
Pranai-1 <reddypranai2017@gmail.com>
2023-08-05 12:31:52 +0530

    JS to TS

---
+++ week-7/assignment-1/middleware/index.ts
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { NextFunction, Response, Request} from 'express';
+
+export const SECRET = 'SECr3t';  // This should be in an environment variable in a real application
+
+export const authenticateJwt = (req:Request, res:Response, next:NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      if(user==undefined){
+        return res.sendStatus(403);
+      }
+      if(typeof user==="string"){
+        return res.sendStatus(403);
+      }
+      req.headers["userId"] = user.id;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+
