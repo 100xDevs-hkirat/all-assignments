ee6218c21b806d3bd86a8d2c6b5ed200ab4ed419
Vibhor <vibgitcode27@gmail.com>
2023-08-05 20:25:10 +0530

    Solution of Week-3 simple and medium 1

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require("jsonwebtoken")
 const app = express();
 
 app.use(express.json());
@@ -7,46 +8,131 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const salt = "gen3tok3n2mnk"
+let generateToken = (data) =>
+{
+  let payload = {username : data.username}
+  let token = jwt.sign(payload , salt , {expiresIn : `1h`})
+  return token;
+}
+
+let authMidddleWare = (req ,res, next) =>
+{
+  let authToken = req.headers.authorization;
+  if(authToken)
+  {
+    const authtokenarr = authToken.split(" ")[1];
+    jwt.verify( authtokenarr , salt , (err , data) =>
+    {
+      if(err) throw err;
+      req.user = data;
+      next()
+    })
+  }
+  else
+  {
+    res.status(404).json({message : "Authorization failed"});
+  }
+}
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+  let user = req.body;
+  let check = ADMINS.find(t => t.username == user.username)
+  if(check)
+  {
+    res.status(404).json({message : "Admin already exists"})
+  }
+  else
+  {
+    let token = generateToken(user)
+    res.status(200).json({message : "Signed Up successfully" , token})
+  }
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post('/admin/login', authMidddleWare , (req, res) => {
+  res.status(200).json({message : "You are logged in successfully"})
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+app.post('/admin/courses', authMidddleWare , (req, res) => {
+  // Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }, Body: { title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }
+  // { message: 'Course created successfully', courseId: 1 }
+  let new_course = req.body;
+  new_course.id = Date.now();
+  COURSES.push(new_course);
+  res.status(201).json({message : "Course created successfully" , courseId : new_course.id});
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+app.put('/admin/courses/:courseId', authMidddleWare , (req, res) => {
+    let courseID = req.params.courseId;
+    let updatedCourse = req.body;
+    updatedCourse.id = req.params.courseId;
+    let courseIndex = COURSES.findIndex(t => t.id == courseID)
+    if(courseIndex != -1)
+    {
+      COURSES[courseIndex] = updatedCourse;
+      res.status(200).json(updatedCourse);
+    }
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+app.get('/admin/courses', authMidddleWare , (req, res) => {
+  res.status(200).json(COURSES)
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+  // Description: Creates a new user account.
+  // Input: { username: 'user', password: 'pass' }
+  // Output: { message: 'User created successfully', token: 'jwt_token_here' }
+  let new_user = {...req.body , purchasedCourses : []};
+  let check = USERS.find(t => t.username == req.body.username)
+  if(check)
+  {
+    res.status(404).json({message : "Username is taken"})
+  }
+  else
+  {
+    USERS.push(new_user)
+    let token = generateToken(new_user)
+    res.status(203).json({message : "Signed Up successfully" , token} )
+  }
 });
 
-app.post('/users/login', (req, res) => {
-  // logic to log in user
+app.post('/users/login', authMidddleWare , (req, res) => {
+    res.status(200).json({message : "You aare logged in successfully"})
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+app.get('/users/courses', authMidddleWare , (req, res) => {
+  let check = COURSES.filter(t => t.published)
+  if(check)
+  {
+    res.status(200).json(check)
+  }
+  else
+  {
+    res.status(404).json({message : "No courses available"});
+  }
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+app.post('/users/courses/:courseId', authMidddleWare , (req, res) => {
+  // Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased.
+  //  Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }
+  //  Output: { message: 'Course purchased successfully' }
+  let courseID = req.params.courseId;
+  let foundCourse = COURSES.find(t => t.id == courseID)
+  if(foundCourse)
+  {
+    req.user.purchasedCourses.push(foundCourse);
+    res.status(200).json(foundCourse)
+  }
+  else
+  {
+    res.status(404).json({message : "Unable to purchase courses"})
+    
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+app.get('/users/purchasedCourses', authMidddleWare , (req, res) => {
+    res.status(200).json(purchasedCourses)
 });
 
 app.listen(3000, () => {
--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -7,46 +7,145 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+let authMiddleWareAdmin = (req ,res, next) =>
+{
+  const { username, password } = req.headers;
+    let check = ADMINS.find(t => t.username == username && t.password == password)
+    if(check == null)
+    {
+      res.status(404).send("Check username and password")
+    }
+    else
+    {
+      next();
+    }
+}
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+    let admin = req.body;
+    let check = ADMINS.find(a => a.username == admin.username)
+    if(check == null)
+    {
+      ADMINS.push(admin);
+      res.status(201).json(admin)
+    }
+    else
+    {
+      res.status(404).send("Username already exist");
+    }
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post('/admin/login', authMiddleWareAdmin , (req, res) => {
+  res.status(201).send("LOGIN successful")
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+app.post('/admin/courses', authMiddleWareAdmin , (req, res) => {
+  let course = req.body;
+  course.id = Date.now();
+  COURSES.push(course);
+  res.status(200).send({ message: 'Course created successfully', courseId: course.id}); 
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+app.put('/admin/courses/:courseId', authMiddleWareAdmin , (req, res) => {
+  let foundcourse = COURSES.find(i => i.id == req.params.courseId)
+  if(foundcourse == null)
+  {
+    res.send("Cannot find course information")
+  }
+  else
+  {
+    Object.assign(foundcourse , req.body)
+    res.status(201).json(foundcourse)
+  }
 });
 
-app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+app.get('/admin/courses', authMiddleWareAdmin , (req, res) => {
+  // Input: Headers: { 'username': 'admin', 'password': 'pass' }
+  //  Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+  res.status(201).json({ courses : COURSES})
 });
+
+let authMiddleWareUser = (req ,res ,next) =>
+{
+  let {username , password} = req.headers;
+  let check = USERS.find(t => t.username == username && t.password == password)
+  if(check == null)
+  {
+    res.status(404).send("Check username and Password")
+  }
+  else
+  {
+    req.user = check;
+    next();
+  }
+}
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+  let new_user = {...req.body , purchasedCourses : []};
+  // or
+  // let new_user = 
+  // {
+  //   username : req.body.username ,
+  //   password : req.body.password ,
+  //   purchasedCourses : []
+  // }
+  let check = USERS.find(t => t.username == req.body.username)
+  if(check == null)
+  {
+    USERS.push(new_user)
+    res.status(201).json({ message: 'User created successfully' })
+  }
+  else
+  {
+    res.status(404).send("Username is taken")
+  }
 });
 
-app.post('/users/login', (req, res) => {
-  // logic to log in user
+app.post('/users/login', authMiddleWareUser ,(req, res) => {
+  res.status(200).json({ message: 'Logged in successfully' })
 });
 
-app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+app.get('/users/courses', authMiddleWareUser , (req, res) => {
+  let filtered = COURSES.filter(t => t.published) 
+  if(filtered)
+  {
+    res.status(201).json(filtered);
+  }
+  else{
+    res.status(404).send("NO, courses to purchase")
+  }
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+app.post('/users/courses/:courseId', authMiddleWareUser , (req, res) => {
+  // Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased.
+  //  Input: Headers: { 'username': 'admin', 'password': 'pass' }
+  //  Output: { message: 'Course purchased successfully' }
+  let courseID = req.params.id;
+
+  let course = COURSES.find(t => t.courseId == courseID && t.published)
+  if(course)
+  {
+    req.user.purchasedCourses.push(course.id);
+    res.status(200).json(course)
+  }
+  else
+  {
+    res.status(404).send("Course not available")
+  }
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+app.get('/users/purchasedCourses', authMiddleWareUser , (req, res) => {
+    let purchasedCourseIds = req.user.purchasedCourses.id;
+    let purchases = [];
+    for (let index = 0; index < COURSES.length; index++)
+    {
+      if(purchasedCourseIds.indexOf(COURSES[i].id) !== -1){
+        purchases.push(COURSES[i])
+      }
+    }
+    res.status(200).json(purchases);
 });
 
 app.listen(3000, () => {
