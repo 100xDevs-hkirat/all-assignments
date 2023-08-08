8dcbf5f2931bb1dee0196d1efc81a1aed923b873
codescalper <msfunbook@gmail.com>
2023-08-08 16:29:20 +0530

    Done with todos

--- week-2/Week-2-Assignments/02-nodejs/fileServer.js
+++ week-2/Week-2-Assignments/02-nodejs/fileServer.js
@@ -18,7 +18,7 @@
  */
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); 
 const app = express();
 
 
--- week-2/Week-2-Assignments/02-nodejs/todoServer.js
+++ week-2/Week-2-Assignments/02-nodejs/todoServer.js
@@ -44,6 +44,81 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+function findIndex(arr,id){
+    for(var i of arr){
+      if(i.id==id) return arr.indexOf(i);
+      
+    }
+    return -1;
+
+}
+
+function deleteItemIndex(arr,id){
+  var newTodo =[]
+  for(var i=0;i<arr.length;i++){
+    if(arr[i].id !== id) newTodo.push(arr[i]);
+  }
+  return newTodo;
+}
+
 app.use(bodyParser.json());
+
+var todo = []
+
+app.get('/todos',(req,res)=>{
+    res.status(200).send(todo);
+})
+
+app.get('/todos/:id', (req, res) => {
+  var todoIndex = findIndex(todo, parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    res.json(todo[todoIndex]);
+  }
+});
+var counter = 1;
+app.post('/todos',(req,res)=>{
+  
+  var newTodo = {
+    id:counter++,
+    title:req.body.title,
+    description:req.body.description
+  }
+  todo.push(newTodo)
+  res.status(201).send(newTodo)
+})
+
+app.delete('/todos/:id',(req,res)=>{
+  const id = parseInt(req.params.id);
+  todo = deleteItemIndex(todo, id);
+
+  res.status(200).json(todo);
+})
+
+app.put('/todos/:id',(req,res)=>{
+  const todoIndex = findIndex(todo, parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send('Todo item not found.');
+  } else {
+ 
+    if (req.body.title) {
+      todo[todoIndex].title = req.body.title
+    }
+    if (req.body.description) {
+      todo[todoIndex].description = req.body.description
+    }
+    res.status(200).json(todo[todoIndex])
+  }
+})
+
+app.use('*',(req,res)=>{
+  res.status(404).send('Route not defined')
+})
+
+
+app.listen(3000,()=>{
+  console.log(`Listening at http://localhost:3000`)
+})
 
 module.exports = app;
