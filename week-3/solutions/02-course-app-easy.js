d840ba57d55d052aac0731e21195b2cefabeed5c
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2023-08-09 15:25:00 +0530

    Update 02-course-app-easy.js

--- week-3/solutions/02-course-app-easy.js
+++ week-3/solutions/02-course-app-easy.js
@@ -108,7 +108,7 @@ app.get('/users/purchasedCourses', userAuthentication, (req, res) => {
   // const purchasedCourses = COURSES.filter(c => req.user.purchasedCourses.includes(c.id));
   // We need to extract the complete course object from COURSES
   // which have ids which are present in req.user.purchasedCourses
-  var purchasedCourseIds = req.user.purchasedCourses; [1, 4];
+  var purchasedCourseIds = req.user.purchasedCourses; 
   var purchasedCourses = [];
   for (let i = 0; i<COURSES.length; i++) {
     if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
