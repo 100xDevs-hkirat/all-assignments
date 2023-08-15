53d3efa4a69a3e4e650d3c05f87a3b208111885d
Prabhat Dwivedi <96712395+dwi11harsh@users.noreply.github.com>
2023-08-15 19:25:23 +0530

    added solutions to anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,30 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  let arr1 = [];
+  let arr2 = [];
+
+  for (let i = 0; i < str1.length; i++) {
+    arr1.push(str1[i]);
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    arr2.push(str2[i]);
+  }
+
+  arr1.sort();
+  arr2.sort();
+
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
