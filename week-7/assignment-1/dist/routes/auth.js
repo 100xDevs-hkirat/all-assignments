b154a09cd161f78d2edd5a836e8f6ea70af3fe11
Sohan <sohan.j12@gmail.com>
2023-08-17 12:31:20 +0530

    added zod validation for signup and post todo routes

--- week-7/assignment-1/dist/routes/auth.js
+++ week-7/assignment-1/dist/routes/auth.js
@@ -16,38 +16,58 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const express_1 = __importDefault(require("express"));
 const middleware_1 = require("../middleware/");
 const db_1 = require("../db");
+const zod_1 = require("zod");
 const router = express_1.default.Router();
-router.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const emailSchema = zod_1.z.string().email("Invalid email format");
+const passwordSchema = zod_1.z
+    .string()
+    .min(8, "Password should be atleast 8 characters long")
+    .max(50, "Password cannot exceed 50 characters")
+    .regex(/^(?=.*[a-zA-Z])(?=.*[0-9])/u, "Must contain letters and numbers");
+router.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { username, password } = req.body;
-    const user = yield db_1.User.findOne({ username });
-    if (user) {
-        res.status(403).json({ message: 'User already exists' });
+    try {
+        const validEmail = emailSchema.parse(username);
+        const validPassword = passwordSchema.parse(password);
+        const user = yield db_1.User.findOne({ validEmail });
+        if (user) {
+            res.status(403).json({ message: "User already exists" });
+        }
+        else {
+            const newUser = new db_1.User({ validEmail, validPassword });
+            yield newUser.save();
+            const token = jsonwebtoken_1.default.sign({ id: newUser._id }, middleware_1.SECRET, { expiresIn: "1h" });
+            res.json({ message: "User created successfully", token });
+        }
     }
-    else {
-        const newUser = new db_1.User({ username, password });
-        yield newUser.save();
-        const token = jsonwebtoken_1.default.sign({ id: newUser._id }, middleware_1.SECRET, { expiresIn: '1h' });
-        res.json({ message: 'User created successfully', token });
+    catch (e) {
+        if (e instanceof zod_1.z.ZodError) {
+            const errorMessage = e.errors[0].message;
+            res.status(400).json({ message: errorMessage });
+        }
+        else {
+            res.status(400).json({ message: "Signup failed" });
+        }
     }
 }));
-router.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { username, password } = req.body;
     const user = yield db_1.User.findOne({ username, password });
     if (user) {
-        const token = jsonwebtoken_1.default.sign({ id: user._id }, middleware_1.SECRET, { expiresIn: '1h' });
-        res.json({ message: 'Logged in successfully', token });
+        const token = jsonwebtoken_1.default.sign({ id: user._id }, middleware_1.SECRET, { expiresIn: "1h" });
+        res.json({ message: "Logged in successfully", token });
     }
     else {
-        res.status(403).json({ message: 'Invalid username or password' });
+        res.status(403).json({ message: "Invalid username or password" });
     }
 }));
-router.get('/me', middleware_1.authenticateJwt, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/me", middleware_1.authenticateJwt, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const user = yield db_1.User.findOne({ _id: req.headers["userId"] });
     if (user) {
         res.json({ username: user.username });
     }
     else {
-        res.status(403).json({ message: 'User not logged in' });
+        res.status(403).json({ message: "User not logged in" });
     }
 }));
 exports.default = router;
