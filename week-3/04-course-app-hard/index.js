4531bd7828f2d703ff396412ee790c275bdcab78
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-18 12:48:02 +0530

    Hard week3 changes

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -176,14 +176,16 @@ app.post("/users/courses/:courseId", authenticateJwt, async (req, res) => {
 	}
 });
 
-app.get("/users/purchasedCourses", (req, res) => {
+app.get("/users/purchasedCourses", async (req, res) => {
 	// logic to view purchased courses
-  const user = await User.findOne({ username: req.user.username }).populate('purchasedCourses');
-  if (user) {
-    res.json({ purchasedCourses: user.purchasedCourses || [] });
-  } else {
-    res.status(403).json({ message: 'User not found' });
-  }
+	const user = await User.findOne({ username: req.user.username }).populate(
+		"purchasedCourses"
+	);
+	if (user) {
+		res.json({ purchasedCourses: user.purchasedCourses || [] });
+	} else {
+		res.status(403).json({ message: "User not found" });
+	}
 });
 
 app.listen(3000, () => {
