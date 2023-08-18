4994870abcf297d8ea2b4c04e6fafc0c3032ffc2
Satyaaam <sattu62025@gmail.com>
2023-08-18 22:54:51 +0530

    solved the authentication server part

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,23 @@
 */
 
 function isAnagram(str1, str2) {
+  str1.toLowerCase();
+  str2.toLowerCase();
+    const str1array = str1.split("");
+    const str2array = str2.split("");
 
+    str1array.sort();
+    str2array.sort();
+    const str1joined = str1array.join("");
+    const str2joined = str2array.join("");
+    if(str1joined === str2joined){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 module.exports = isAnagram;
+
+isAnagram("ajar","raja")
