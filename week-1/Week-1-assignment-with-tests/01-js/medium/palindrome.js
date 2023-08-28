7f05ffac60cc99f2f8e3710878e90c0b0b9e599a
Rohankupate <kupaterohan@gmail.com>
2023-08-28 11:04:44 +0530

    commit all

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -5,9 +5,38 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function transform(str ){
+  arr = str.split('');
+  var str2 ='';
+  for( i of arr){
+    if (i===',' || i ==='!'|| i ==='?'|| i ===''|| i ==='.'){
 
+    }
+    else{
+str2+=i;
+    }
+  }
+  return str2 ;
+}
 function isPalindrome(str) {
-  return true;
+ 
+  // var str1 = str.toLowerCase().split(' ').join('')
+  var str1 = transform(str).toLowerCase().split(' ').join('')
+  var arr = str1.split('')
+  var ans ='';
+  var count = 0;
+  for (let i =arr.length-1 ; i>=0 ; i--){
+    
+    
+      ans += arr[i];
+    
+    // console.log(!/^[a-zA-Z0-9]$/.test(arr[i]))
+  }
+ 
+  console.log(str1)
+  console.log(ans)
+ return str1==ans;
 }
 
+console.log(isPalindrome('Mr. Owl ate my metal worm.'))
 module.exports = isPalindrome;
