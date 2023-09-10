c6edae3673d820f99ac17d6524e3326aefe7ea1d
Neeru Kapoor <467neeru@gmail.com>
2023-09-10 22:50:25 +0530

    Completed week 3 easy and medium assignments

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,23 +1,83 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+let AdminSchema = new mongoose.Schema({
+  username: {
+    type:String,
+  },
+  password: {
+    type: Number
+  }
+});
+
+let UsersSchema = new mongoose.Schema({
+  username: {
+    type:String,
+  },
+  password: {
+    type: Number
+  },
+  coursesPurchased: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Courses'
+  }
+});
+let CoursesSchema = new mongoose.Schema({
+  courseId: Number,
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UsersSchema);
+const Courses = mongoose.model('Courses', CoursesSchema);
+
+const adminAuth = (req,res,next) => {
+  const username = req.body.username;
+  const pass = req.body.pass;
+  const admin = Admin.findOne({'username': username, 'password':pass});
+  if(admin) {
+    res.json({message: "Admin Authenticated Successfully"});
+  } else {
+    res.json({message: "Admin Failed Authenticatino"});
+  }
+}
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const username = req.body.username;
+  const pass = req.body.pass;
+  const admin = new Admin({
+    username: username,
+    password: pass
+  });
+  admin.save();
 });
 
-app.post('/admin/login', (req, res) => {
+app.post('/admin/login', adminAuth, (req, res) => {
   // logic to log in admin
+  res.json({message: "Logged in successfully"})
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', adminAuth, (req, res) => {
   // logic to create a course
+  const newCourse = req.body;
+  const course = new Courses({
+    courseId: Courses.length + 1,
+    title:newCourse.title,
+    description:newCourse.description,
+    price:newCourse.price,
+    imageLink:newCourse.imageLink,
+    published:newCourse.imageLink
+  })
+  course.save();
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
