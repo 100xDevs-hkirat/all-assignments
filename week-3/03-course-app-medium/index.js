9cd107109067122e91dd087c20bb69ffb161f679
yashbudhia <yeshbudhia@gmail.com>
2023-09-14 20:15:47 +0530

    Week 3 assignments (self attempted)

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const app = express();
+const fs = require("fs/promises");
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const { executionAsyncId } = require('async_hooks');
+const { read } = require('fs');
 
+
+//File path variables
+const adminFilePath = path.join(__dirname,"admins.json");
+const userFilePath = path.join(__dirname, "users.json");
+const coursesFilePath = path.join(__dirname, "courses.json");
+const purchasesFilePath = path.join(__dirname, "purchases.json");
+
+//Read and write functions
+async function readAdminFile() {
+  const data = await fs.readFile(adminFilePath,'utf-8');
+  return JSON.parse(data);
+}
+async function writeFile(filepath,data) {
+  fs.writeFile(filepath,JSON.stringify(data));
+}
+app.use(express.static(__dirname));
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.get("/admin/signup",(req,res) => {
+  res.sendFile(path.join(__dirname,"adminsignup.html"));
+})
+
+app.post('/admin/signup', async (req, res) => {
   // logic to sign up admin
+  try {
+    const adminfiledata = await readAdminFile();
+    
+    const signin = req.body;
+
+    signin.id = adminfiledata.length + 1; // Fix the length calculation
+    adminfiledata.push(signin);
+    await writeFile(adminFilePath, adminfiledata); // Use the writeFile function
+    res.status(201).json(adminfiledata); // Send a response
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
 });
 
 app.post('/admin/login', (req, res) => {
@@ -48,6 +89,10 @@ app.post('/users/courses/:courseId', (req, res) => {
 app.get('/users/purchasedCourses', (req, res) => {
   // logic to view purchased courses
 });
+
+app.get('*', (req, res) => {
+  res.redirect('/')
+})
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
