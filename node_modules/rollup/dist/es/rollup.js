dea1689c355bedf74de26e5339301ae77b3d6180
Neeru Kapoor <467neeru@gmail.com>
2023-09-25 22:00:57 +0530

    Created new project for users to purchase course and added npm dependencies

---
+++ node_modules/rollup/dist/es/rollup.js
@@ -0,0 +1,17 @@
+/*
+  @license
+	Rollup.js v3.29.3
+	Sun, 24 Sep 2023 05:31:36 GMT - commit 353e4628284b1ef4d8a8b47d895881e5ce6546f4
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+export { version as VERSION, defineConfig, rollup, watch } from './shared/node-entry.js';
+import 'node:path';
+import 'path';
+import 'node:process';
+import 'node:perf_hooks';
+import 'node:crypto';
+import 'node:fs/promises';
+import 'tty';
---
+++ node_modules/rollup/dist/shared/fsevents-importer.js
@@ -0,0 +1,37 @@
+/*
+  @license
+	Rollup.js v3.29.3
+	Sun, 24 Sep 2023 05:31:36 GMT - commit 353e4628284b1ef4d8a8b47d895881e5ce6546f4
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+let fsEvents;
+let fsEventsImportError;
+async function loadFsEvents() {
+    try {
+        ({ default: fsEvents } = await import('fsevents'));
+    }
+    catch (error) {
+        fsEventsImportError = error;
+    }
+}
+// A call to this function will be injected into the chokidar code
+function getFsEvents() {
+    if (fsEventsImportError)
+        throw fsEventsImportError;
+    return fsEvents;
+}
+
+const fseventsImporter = /*#__PURE__*/Object.defineProperty({
+    __proto__: null,
+    getFsEvents,
+    loadFsEvents
+}, Symbol.toStringTag, { value: 'Module' });
+
+exports.fseventsImporter = fseventsImporter;
+exports.loadFsEvents = loadFsEvents;
+//# sourceMappingURL=fsevents-importer.js.map
