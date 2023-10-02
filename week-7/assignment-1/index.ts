7229d7ab685ce10bff4f725c94cbe43997ce4d38
yashbudhia <yeshbudhia@gmail.com>
2023-10-02 21:07:49 +0530

    typescript asignment -1

---
+++ week-7/assignment-1/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+const port = 3000;
+import authRoutes from "./routes/auth";
+import todoRoutes from "./routes/todo";
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+mongoose.connect(
+  "mongodb+srv://yashbudhia:khuljas1ms1m@cluster0.nnafmtq.mongodb.net/",
+  { dbName: "courses" }
+);
