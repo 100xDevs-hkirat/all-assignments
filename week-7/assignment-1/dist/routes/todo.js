771c55af4bba9eb025142258aea07fa3abb370fc
saadabban76 <saadabban76@gmail.com>
2023-10-07 02:07:56 +0530

    week 7 assignment done !

---
+++ week-7/assignment-1/dist/routes/todo.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const { authenticateJwt, SECRET } = require("../middleware/index");
+const { Todo } = require("../db");
+const router = express.Router();
+router.post('/todos', authenticateJwt, (req, res) => {
+    const { title, description } = req.body;
+    const done = false;
+    const userId = req.userId;
+    const newTodo = new Todo({ title, description, done, userId });
+    newTodo.save()
+        .then((savedTodo) => {
+        res.status(201).json(savedTodo);
+    })
+        .catch((err) => {
+        res.status(500).json({ error: 'Failed to create a new todo' });
+    });
+});
+router.get('/todos', authenticateJwt, (req, res) => {
+    const userId = req.userId;
+    Todo.find({ userId })
+        .then((todos) => {
+        res.json(todos);
+    })
+        .catch((err) => {
+        res.status(500).json({ error: 'Failed to retrieve todos' });
+    });
+});
+// ? patch : it is similar to put 
+router.patch('/todos/:todoId/done', authenticateJwt, (req, res) => {
+    const { todoId } = req.params;
+    const userId = req.userId;
+    Todo.findOneAndUpdate({ _id: todoId, userId }, { done: true }, { new: true })
+        .then((updatedTodo) => {
+        if (!updatedTodo) {
+            return res.status(404).json({ error: 'Todo not found' });
+        }
+        res.json(updatedTodo);
+    })
+        .catch((err) => {
+        res.status(500).json({ error: 'Failed to update todo' });
+    });
+});
+module.exports = router;
+// const express = require("express");
+// const { authenticateJwt, SECRET } = require("../middleware/index");
+// const { Todo } = require("../db");
+// const router = express.Router();
+// // Route to Post the todo to database
+// router.post("/todos", authenticateJwt, async (req, res) => {
+//   const {title} = req.body;
+//   const todoExist = await Todo.findOne({ title });
+//   console.log("todoExist : ", todoExist);
+//   if (todoExist) {
+//     res.status(401).send("this todo already exists !");
+//   } else {
+//     const newTodo = await new Todo(req.body);
+//     await newTodo.save();
+//     res.json({
+//       todo: req.body,
+//       message: "Todo Added Sucesfully !",
+//     });
+//   }
+// });
+// // Route to Get the Todos from database
+// router.get('/todos', authenticateJwt, async (req, res) => {
+//   await Todo.find({}).then((data) => {
+//     res.json({todos: data});
+//   }).catch((err) => {
+//     res.status(404).send(err);
+//   })
+// })
+// module.exports = router;
