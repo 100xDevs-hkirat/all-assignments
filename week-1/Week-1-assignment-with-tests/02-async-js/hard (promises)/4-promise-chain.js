96c070cb8f0a91f2f5cf6e3632170dcbf34ea2ea
Aditya Barodiya <98759220+adityabarodiya@users.noreply.github.com>
2023-11-06 19:50:01 +0530

    Update 4-promise-chain.js

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
@@ -1,22 +1,51 @@
-/*
- * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
- * Write a function that sequentially calls all 3 of these functions in order.
- * Print out the time it takes to complete the entire operation.
- * Compare it with the results from 3-promise-all.js
- */
 
-function waitOneSecond() {
+  /*
+   * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
+   * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
+   * Print how long it took for all 3 promises to resolve.
+   */
 
-}
+  const { log } = require("console");
 
-function waitTwoSecond() {
+  function waitOneSecond() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("one second has passed");
+        resolve();
+      }, 1000);
+    });
+  }
 
-}
+  function waitTwoSecond() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("two second has passed");
+        resolve();
+      }, 2000);
+    });
+  }
 
-function waitThreeSecond() {
+  function waitThreeSecond() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("three second has passed");
+        resolve();
+      }, 3000);
+    });
+  }
 
-}
+  function calculateTime() {
+    const before = new Date().getTime();
+    waitOneSecond().then(() => {
+      waitTwoSecond().then(() => {
+        waitThreeSecond().then(() => {
+          const after = new Date().getTime();
+          const time = after - before;
+          console.log(`Time taken: ${time} ms`);
+        });
+      });
+    });
+  }
 
-function calculateTime() {
+  calculateTime();
 
-}
