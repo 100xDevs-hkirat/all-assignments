39b8a8d9e4341eddc883d8d923a2bf8702c1d4ff
Abhilash <abhilash301267@gmail.com>
2023-11-06 19:09:33 +0530

    Learnt how components and states work in react. Gave a simple state variable as input propes into a component, and then rendered it

--- react-basics/front-end-todo-framework/src/App.jsx
+++ react-basics/front-end-todo-framework/src/App.jsx
@@ -3,31 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todo, setTodo] = useState({
+    id:"hhuihuhuiiu004994",
+    title: "Go to gym",
+    description: "Gym karo, bode banao, khush raho"
+  })
+  setInterval(()=>{
+    setTodo(({
+      id:"hhuihuhuiiu004994",
+      title: "Eat food",
+      description: "Acha khaana khao"
+    }))
+  },3000);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo App</h1>
+      <UserName todoTitle = {todo.title} todoDescription = {todo.description}></UserName>
+    </>
+  )
+}
+
+function UserName(props){
+  return (
+    <>
+    <div>
+      {props.todoTitle}<br></br>{props.todoDescription}
+    </div>
     </>
   )
 }
