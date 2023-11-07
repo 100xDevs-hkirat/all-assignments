7ff002f204213601528c6f5e2835ef4bd74aa3d9
Abhilash <abhilash301267@gmail.com>
2023-11-07 14:57:13 +0530

    understood how to render arras, and thus, know how to execute js code inside the html block in jsx

--- react-basics/front-end-todo-framework/src/App.jsx
+++ react-basics/front-end-todo-framework/src/App.jsx
@@ -8,34 +8,41 @@ import './App.css'
 
 function App() {
 
-  const [todo, setTodo] = useState({
-    id:"hhuihuhuiiu004994",
+  const [todos, setTodo] = useState([{
+    id: "hhuihuhuiiu004994",
     title: "Go to gym",
     description: "Gym karo, bode banao, khush raho"
-  })
-  setInterval(()=>{
-    setTodo(({
-      id:"hhuihuhuiiu004994",
-      title: "Eat food",
-      description: "Acha khaana khao"
-    }))
-  },3000);
-
+  },
+  {
+    id: ",jfgtrhrhhyyhhr",
+    title: "Eat Food",
+    description: "Gym karo, bode banao, khush raho"
+  },
+  {
+    id: "mmjjmjmjjj",
+    title: "thnrr",
+    description: "Gym karo, bode banao, khush raho"
+  }])
   return (
     <>
-      <h1>Todo App</h1>
-      <UserName todoTitle = {todo.title} todoDescription = {todo.description}></UserName>
+      {todos.map(todo => {
+        return (
+          <Todo title = {todo.title} description = {todo.description}></Todo>
+        )
+      })}
     </>
   )
 }
 
-function UserName(props){
+function Todo(props) {
   return (
-    <>
-    <div>
-      {props.todoTitle}<br></br>{props.todoDescription}
+    <div style={{backgroundColor: "blue"}}>
+      {props.title}
+      <br></br>
+      {props.description}
+      <br></br>
+      <br></br>
     </div>
-    </>
   )
 }
 
