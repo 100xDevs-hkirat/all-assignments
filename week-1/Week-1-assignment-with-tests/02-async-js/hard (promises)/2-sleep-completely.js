6f63a9cba4ddd17817b2cfa09909837a1bb54cf0
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-11-16 18:10:16 +0530

    sleep-completely

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/2-sleep-completely.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,14 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
+
+async function exampleUsage() {
+    console.log("start");
+    await sleep(3);
+    console.log("End");
+}
+
+exampleUsage();
