27aa83f470d27e11b9f580cd64ce1a46102c9829
Zahid <zahidmohammad495@gmai.com>
2023-11-26 16:32:47 +0530

    complete week 3.4, course-easy-app assignment

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -7,48 +7,103 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+// admin authentication middleware
+const adminAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+
+  const admin = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (admin) next();
+  else res.status(403).json({ message: "Admin authentication failed" });
+};
+
+// user authentication middleware
+const userAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  const user = USERS.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (user) next();
+  else res.status(403).json({ message: "User authentication failed" });
+};
+
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post("/admin/signup", (req, res) => {
   // logic to sign up admin
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) res.status(403).json({ message: "Admin already exist" });
+  else {
+    ADMINS.push(admin);
+    res.json({ message: "Admin created successfully" });
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post("/admin/login", adminAuthentication, (req, res) => {
   // logic to log in admin
+  res.json({ message: "Logged in successfully" });
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post("/admin/courses", adminAuthentication, (req, res) => {
   // logic to create a course
+  const course = req.body;
+  course.id = Date.now();
+  COURSES.push(course);
+  res.json({ message: "Course created successfully", courseID: course.id });
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   // logic to edit a course
+  const courseID = parseInt(req.params.courseId);
+  const course = COURSES.find((c) => c.id === courseID);
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: "Course updates successfully" });
+  } else res.status(403).json({ message: "Course not found" });
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", adminAuthentication, (req, res) => {
   // logic to get all courses
+  res.json({ course: COURSES });
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", (req, res) => {
   // logic to sign up user
+  const user = { ...req.body, purchasedCourses: [] };
+  USERS.push(user);
+  res.json({ message: "User created successfully" });
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", userAuthentication, (req, res) => {
   // logic to log in user
+  res.json({ message: "Logged in successfully" });
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", userAuthentication, (req, res) => {
   // logic to list all courses
+  res.json({ course: COURSES.filter((c) => c.filter) });
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", userAuthentication, (req, res) => {
   // logic to purchase a course
+  const courseID = Number(req.params.courseId);
+  const course = COURSES.find((c) => c.courseID === courseID && c.published);
+  if (course) {
+    req.user.purchasedCourses.push(course);
+    res.json({ message: "Course purchased successfully" });
+  } else res.status(404).json({ message: "Course not found or not available" });
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", userAuthentication, (req, res) => {
   // logic to view purchased courses
+  const purchasedCourses = COURSES.filter((c) =>
+    req.user.purchasedCourses.includes(c.id)
+  );
+  res.json({ purchasedCourses });
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
