602660dfecb0a36f6f9496b7003cf47486761a48
Zahid <zahidmohammad495@gmai.com>
2023-11-28 13:23:43 +0530

    Complete week 3.4 assignment on course-app-hard

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,54 +1,171 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(express.json());
+const SECRET = "mineS3cr3t";
 
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+// Define mongoose schemas
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+});
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean,
+});
+
+// Define mongoose models
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+
+// authentication middleware
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
+  } else res.sendStatus(401);
+};
+
+// connct to mongodb through url
+mongoose.connect(
+  "mongodb+srv://zahid:zahid495@cluster0.t7uye72.mongodb.net/courses",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "courses",
+  }
+);
+
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post("/admin/signup", async (req, res) => {
   // logic to sign up admin
+  const { username, password } = req.body;
+  const admin = await Admin.findOne({ username });
+  if (admin) res.send(403).json({ message: "Admin already exist" });
+  else {
+    const newAdmin = new Admin({ username, password });
+    await newAdmin.save();
+    const token = jwt.sign({ username, role: "admin" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Admin created successfully", token });
+  }
 });
 
-app.post('/admin/login', (req, res) => {
+app.post("/admin/login", async (req, res) => {
   // logic to log in admin
+  const { username, password } = req.headers;
+  const admin = Admin.findOne({ username, password });
+  if (admin) {
+    const token = jwt.sign({ username, role: "admin" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Logged in successfully", token });
+  } else res.status(403).json({ message: "Invalid username or password" });
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post("/admin/courses", authenticateJwt, async (req, res) => {
   // logic to create a course
+  const course = new Course(req.body);
+  await course.save();
+  res.json({ message: "Course created successfully", courseID: course.id });
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", authenticateJwt, async (req, res) => {
   // logic to edit a course
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+    new: true,
+  });
+  if (course) res.json({ message: "Course updated successfully" });
+  else res.status(404).json({ message: "Course not found" });
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", authenticateJwt, async (req, res) => {
   // logic to get all courses
+  const courses = await Course.find({});
+  res.json({ courses });
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", async (req, res) => {
   // logic to sign up user
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user) res.status(403).json({ message: "User already exist" });
+  else {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "User created Successfully" });
+  }
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", async (req, res) => {
   // logic to log in user
+  const { username, password } = req.headers;
+  const user = await User.findOne({ username, password });
+  if (user) {
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Logged in successfully", token });
+  } else res.status(403).json({ message: "Invalid username or password" });
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", authenticateJwt, async (req, res) => {
   // logic to list all courses
+  const courses = await Course.find({ published: true });
+  res.json({ courses });
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", authenticateJwt, async (req, res) => {
   // logic to purchase a course
+  const course = await Course.findById(req.params.courseId);
+  if (course) {
+    const user = await User.findOne({ username: req.user.username });
+    if (user) {
+      user.purchasedCourses.push(course);
+      await user.save();
+      res.json({ message: "Course purchased successfully" });
+    } else res.status(403).json({ message: "User not found" });
+  } else res.status(404).json({ message: "Course not found" });
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", authenticateJwt, async (req, res) => {
   // logic to view purchased courses
+  const user = await User.findOne({ username: req.user.username }).populate(
+    "purchasedCourses"
+  );
+  if (user) {
+    res.json({ purchasedCourses: user.purchasedCourses || [] });
+  } else res.status(403).json({ message: "User not found" });
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
