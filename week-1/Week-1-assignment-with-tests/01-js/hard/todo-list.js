bd85f63d211fcb0c7d369302caeb6640c0125b61
kiranloka <kirankumarloka@gmail.com>
2023-12-06 01:14:39 +0530

    todos

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -11,8 +11,67 @@
   - `npm run test-todo-list`
 */
 
-class Todo {
+class Todo{
+  constructor(){
+    this.todos=[];
+  }
+  add(todo){
+    this.todo.push(todos);
+  }
+  remove(indexOfTodo){
+    if(indexOfTodo>=0 && indexOfTodo<this.todos.length){
+      this.todos.slice(indexOfTodo,1);
+    }
+    else{
+      console.error('Invalid index set for removing');
+    }
+  }
 
+  update(index,updatedTodo){
+    if(index>=0 && index<this.todos.length){
+      this.todos[index]=updatedTodo;
+    }
+    else{
+      console.error('invalid index to update');
+    }
+  }
+
+  getAll(){
+    return this.todos;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo>=0 && indexOfTodo<this.todos.length){
+      return this.todos[indexOfTodo];
+    }else{
+      console.error('Invalid index provided for retrival');
+      return null;
+    }
+
+  }
+
+  clear(){
+    this.todos=[];
+  }
 }
+
+const myTodoList = new Todo();
+
+myTodoList.add('Task 1');
+myTodoList.add('Task 2');
+myTodoList.add('Task 3');
+
+console.log('All Todos:', myTodoList.getAll());
+
+myTodoList.remove(1);
+console.log('After removing at index 1:', myTodoList.getAll());
+
+myTodoList.update(0, 'Updated Task 1');
+console.log('After updating at index 0:', myTodoList.getAll());
+
+console.log('Task at index 0:', myTodoList.get(0));
+
+myTodoList.clear();
+console.log('After clearing all todos:', myTodoList.getAll());
 
 module.exports = Todo;
