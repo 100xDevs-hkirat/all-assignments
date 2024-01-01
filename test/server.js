4847fdcd0686d5879b7b13bcf9992771cab38fe4
randheer <randheer.dakshana15@gmail.com>
2024-01-01 22:24:19 +0530

    exploring things

---
+++ test/server.js
@@ -0,0 +1,33 @@
+// const express = require("express");
+// port = "3000";
+// const app = express();
+//
+// app.set("view engine", "ejs");
+//
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+//
+// app.get("/hello", (req, res) => {
+//   res.json({ message: " this is hellow world message" });
+// });
+//
+// app.listen(port, () => {
+//   console.log(`server is running at the port ${port}`);
+// });
+//
+
+const students = {
+  name: "sunita",
+  printname: function (){
+    console.log(this.name);
+  }
+}
+
+students.printname()
+
+const student2 = {
+  name: "randheer"
+}
+
+students.printname.call(student2)
