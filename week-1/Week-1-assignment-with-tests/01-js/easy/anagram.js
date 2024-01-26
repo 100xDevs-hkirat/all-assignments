c2ee7230670bc7733abbcdb1bb814d42cf33e5e4
ujjawal396 <ujjawaljha08@gmail.com>
2024-01-26 23:21:00 +0530

    ModifiedEasy

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -6,9 +6,45 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  if(str1.length!=str2.length){
+    return false;
+  }
+
+  let ob={};
+  for(let i=0;i<str1.length;i++){
+    let c=str1.charAt(i);
+    if(c in ob){
+      ob[c]++;
+    }else {
+      ob[c]=1;
+    }
+    
+  }
+
+  for(let i=0;i<str2.length;i++){
+    let c=str2.charAt(i);
+    if(c in ob){
+
+      ob[c]--;
+      if(ob[c]===0){
+        delete ob[c];
+      }
+    
+    }else{
+      return  false;
+    }
+  }
+  return Object.keys(ob).length==0;
+
+  
 
 }
+
+
+
+
 
 module.exports = isAnagram;
